8ea22cab761b5235b46d6f2acbbbc218e018f5f4
Amy Lee <amy.yj.lee@gmail.com>
2017-11-21 15:31:38 -0800

    refactored to have a function, getTrips, that performs the 'get all trips' api request

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-/* eslint-disable */
+
+const getTrips = () => {
+  $.get('https://trektravel.herokuapp.com/trips', (response) => {
+    console.log('success!');
+    response.forEach((trip) => {
+      const tripInfo = `<li><strong id = 'trip-name'>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
+      $('#trip-list ul').append(tripInfo);
+    });
+  }).fail(() => {
+    $('#message').html('<h3> No trips found :( )</h3>');
+  }).always(() => {
+      console.log('Adenture awaits you!');
+  });
+};
+
 $(document).ready(() => {
 
   $('#all-trips').on('click', () => {
-    const allTripsUrl = 'https://trektravel.herokuapp.com/trips';
+    getTrips();
+  });
+  $('#trip-list li').on('click', () => {
+    console.log(`you clicked me! I am a: ${this}`);
+    $.get(showTripURL, (response) => {
+      console.log('you showed the trip!');
 
-    $.get(allTripsUrl, (response) => {
-      console.log('success!');
-      response.forEach((trip) => {
-        const tripInfo = `<li><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
-        $('#trip-list ul').append(tripInfo);
-      });
-    }).fail(() => {
-      $('#message').html('<h3> No trips found :( )</h3>');
-    }).always(() => {
-        console.log('Adenture awaits you!');
-    });
+    })
   });
 });
