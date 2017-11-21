4e61882361a33a793ba57e5571f663ec4fbd3897
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-21 15:17:40 -0800

    Completes wave 2, can click on a trip and see trip details on the page. Needs style but it is function

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const baseUrl = 'https://trektravel.herokuapp.com/trips'
-
+// ' Continent: ' + trip.continent + ' Weeks: ' + trip.weeks +
 $(document).ready(()=> {
+
   let loadTrips = function loadTrips() {
     $.get(baseUrl, (response) => {
       console.log('success!');
-      console.log(response);
-
-      generateHTML = '<ul>'
       response.forEach(function(trip) {
-        console.log(trip);
-        generateHTML += '<li>Name: ' + trip.name + ' Continent: ' + trip.continent + ' Weeks: ' + trip.weeks + '</li>';
+        let tripInfo = `<li><h2 data-id=${trip.id}>` + trip.name + '<h2></li>';
+        $('#show-trips ul').append(tripInfo)
       });
-      generateHTML += '</ul>'
-      $('#show-trips').html(generateHTML);
     })
     .fail(function(response){
       console.log(response);
@@ -23,7 +19,36 @@ $(document).ready(()=> {
     });
   };
 
+  let loadTrip = function loadTrip(id){
+    $.get(baseUrl + `/${id}`, (response) => {
+      console.log('success!');
+      console.log(response);
 
+      let tripInfo = `
+      <p>ID: ${response.id}</p>
+      <p>Category: ${response.category}</p>
+      <p>Destination: ${response.continent}</p>
+      <p>Details: ${response.about}</p>
+      <p>Cost: $${response.cost}</p>
+      <p>Length: ${response.weeks} weeks</p>
+      `;
+      $('#show-trip').html(tripInfo);
+    })
+    .fail(function(response){
+      console.log(response);
+      $('#fail').html('<p>Request was unsuccessful</p>')
+    })
+    .always(function(){
+      console.log('always even if we have success or failure');
+    });
+  };
+
+  // EVENTS
+  $('#show-trips ul').on('click', 'h2', function(){
+    let tripId = $(this).attr('data-id');
+    console.log('show trips event works test test test test');
+    loadTrip(tripId);
+  });
 
   $('#load').on('click', function(){
     loadTrips();
