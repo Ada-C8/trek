4d7fdb4470bbbbdf6259a3ddb0b23f3651db84d3
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-21 14:45:43 -0800

    Allow reservation form to appear

--- index.js
+++ index.js
@@ -3,24 +3,34 @@ $(document).ready(() => {
     $.get(
       'https://trektravel.herokuapp.com/trips',
       (response) => {
-        $('#display').html('');
+        $('#content').html('<div id="content"><ul id="list"></ul></div>');
         for (let i = 0; i < response.length; i += 1) {
           const trip = response[i];
           const weeks = trip.weeks === 1 ? 'week' : 'weeks';
           const tripHTML = `<li><h3><a class='trip' id='${trip.id}' href="#">${trip.name}</a></h3><ul><li>${trip.continent}</li><li>${trip.weeks} ${weeks}</li></ul></li>`;
-          $('#display').append(tripHTML);
+          $('#list').append(tripHTML);
         }
       },
     );
   });
   $('body').on('click', '.trip', (event) => {
-    const id = event.target.id;
     $.get(
-      `https://trektravel.herokuapp.com/trips/${id}`,
+      `https://trektravel.herokuapp.com/trips/${event.target.id}`,
       (response) => {
-        const tripHTML = `<h3>${response.name}</h3><a id='reserve' href='#'>Reserve</a><h4>Details</h4><ul><li>ID: ${response.id}</li><li>Continent: ${response.continent}</li><li>About: ${response.about}</li><li>Category: ${response.category}</li><li>Weeks: ${response.weeks}</li><li>Cost: ${response.cost}</li></ul>`;
-        $('#display').html(tripHTML);
+        const tripHTML = `<a id='reserve' href='#' trip_id='${response.id}'>Reserve</a><h4>Details</h4><ul><li>ID: ${response.id}</li><li>Continent: ${response.continent}</li><li>About: ${response.about}</li><li>Category: ${response.category}</li><li>Weeks: ${response.weeks}</li><li>Cost: ${response.cost}</li></ul>`;
+        $('#title').html(`${response.name}`);
+        $('#content').html(tripHTML);
       },
     );
+  });
+  $('body').on('click', '#reserve', () => {
+    const formElements = ['Name', 'Email', 'Age'];
+    const sectionHTML = formElements.reduce((string, element) => {
+      return `${string}<section><label>${element}</label><input type='text' id='${element.toLowerCase()}' name='${element.toLowerCase()}' /></section>`;
+    }, '');
+    console.log(sectionHTML);
+    const formHTML = `<form>${sectionHTML}<section><button type='submit'>Reserve</button></section></form>`;
+
+    $('#content').html(formHTML);
   });
 });
