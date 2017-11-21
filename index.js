0a1e67aaab0ba164d8808204d71f0255b2bebf8e
averikitsch <aekitsch@gmail.com>
2017-11-21 15:27:56 -0800

    some styling of the list items

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
 
 const tripUrl = 'https://trektravel.herokuapp.com/trips';
-
+const clock = '<i class="fa fa-clock-o" aria-hidden="true"></i>';
+const plane = '<i class="fa fa-plane" aria-hidden="true"></i>';
 const loadTrips = function loadTrips() {
   $.get(tripUrl, (response) => {
     response.forEach((trip) => {
       const name = `<h3>${trip.name}</h3>`;
-      const location = `<li>${trip.continent}</li>`;
-      const time = `<li>${trip.weeks}</li>`;
-      console.log(trip);
+      const location = `<li>${plane} ${trip.continent}</li>`;
+      const week = (trip.week > 1 ? 'weeks' : 'week');
+      const time = `<li>${clock} ${trip.weeks} ${week}</li>`;
+      console.log(location);
       $('#trips').append(`<div class="trip">${name}<ul>${location}${time}</ul></div>`);
     });
   });
