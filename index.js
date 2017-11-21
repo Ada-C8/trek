50e48e74640e6505ba257cbceb44301acaa696c7
Amy Lee <amy.yj.lee@gmail.com>
2017-11-21 15:13:43 -0800

    added fail and always promises to GET alltrips requests, attached the GET allTrips event to the click button event; probably best to refactor this at some point

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /* eslint-disable */
-
 $(document).ready(() => {
 
-  const allTripsUrl = 'https://trektravel.herokuapp.com/trips';
+  $('#all-trips').on('click', () => {
+    const allTripsUrl = 'https://trektravel.herokuapp.com/trips';
 
-  $.get(allTripsUrl, (response) => {
-    response.forEach((trip) => {
-      const tripInfo= `<li><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`
-      $('#trip-list ul').append(tripInfo);
-    })
-
-  })
-
-
-  // $('#all-trips').on('click', () => {
-  //   loadAllTrips();
-  // })
-})
+    $.get(allTripsUrl, (response) => {
+      console.log('success!');
+      response.forEach((trip) => {
+        const tripInfo = `<li><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
+        $('#trip-list ul').append(tripInfo);
+      });
+    }).fail(() => {
+      $('#message').html('<h3> No trips found :( )</h3>');
+    }).always(() => {
+        console.log('Adenture awaits you!');
+    });
+  });
+});
