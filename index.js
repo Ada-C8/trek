659cb0c57e0b6c4a3fd987b7fbe30b23f8c8638a
averikitsch <aekitsch@gmail.com>
2017-11-21 14:59:22 -0800

    all trips print with click

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-// https://trektravel.herokuapp.com/trips
+
+
+const tripUrl = 'https://trektravel.herokuapp.com/trips';
+
+const loadTrips = function loadTrips() {
+  $.get(tripUrl, (response) => {
+    response.forEach((trip) => {
+      const name = `<h3>${trip.name}</h3>`;
+      const location = `<li>${trip.continent}</li>`;
+      const time = `<li>${trip.weeks}</li>`;
+      console.log(trip);
+      $('#trips').append(`<div class="trip">${name}<ul>${location}${time}</ul></div>`);
+    });
+  });
+};
 
 $(document).ready(() => {
-
+  $('#load').on('click', () => {
+    console.log('you clicked this');
+    loadTrips();
+  });
 });
