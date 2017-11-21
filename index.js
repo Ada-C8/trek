9cf3a8f3a4e9634bb6d88bd6683060e729b1b0e0
Gale Harrington <gale.harrington19@gmail.com>
2017-11-21 15:03:13 -0800

    Get list of trips and add to table

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 $(document).ready(() => {
   $('.reveal').click(() => {
     $('.reveal').hide();
-    $('body').append('<table><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
-    // $.get('https://trektravel.herokuapp.com/trips', response => {
-    //
-    // })
+    $('body').append('<table class="trips"><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
+    $.get('https://trektravel.herokuapp.com/trips', response => {
+      response.forEach((trip) => {
+        $('.trips').append('<tr><td>' + trip.name + '</td><td>' + trip.continent + '</td><td>' + trip.weeks + '</td></tr>')
+      })
+    })
   })
 });
