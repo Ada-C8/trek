96ef08ba6e35ac5c6dbc6b5071992e9d04a52390
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-21 21:35:08 -0800

    wave 2: fixed the trip list

--- index.js
+++ index.js
@@ -4,31 +4,30 @@ $(document).ready(() => {
       console.log('success!');
 
       for (i = 0; i < response.length; i++) {
-        let tripInfo = `<article data-id="${response[i].id}" data-name="${response[i].name}" data-continent="${response[i].continent}" data-weeks="${response[i].weeks}" >
-        <ul>
-        <p>${response[i].name}</p>
-        <button class="trip_details">View Trip Details</button>
-        </ul>
-        </article>`
+        let tripInfo = `<article data-id="${response[i].id}" data-name="${response[i].name}" data-continent="${response[i].continent}" data-weeks="${response[i].weeks}">
 
+          <ul>
+            <li>${response[i].name}</li>
+            <button class="trip_details">View Trip Details</button>
+          </ul>
+        </article>`
         $('#trips').append(tripInfo);
       }
 
       $('.trip_details').on('click', function () {
-        let pTag = $(this).siblings()[0];
+
+        let list = $(this).siblings()[0];
 
         let parentArticle = $(this).closest('article');
 
-        let details = `<li>${parentArticle.attr('data-id')}</li>
-          <li>${parentArticle.attr('data-weeks')}</li>
-          <li>${parentArticle.attr('data-continent')}</li>`
+        let details = `<li>${parentArticle.attr("data-id")}</li>
+          <li>${parentArticle.attr("data-weeks")}</li>
+          <li>${parentArticle.attr("data-continent")}</li>`
 
-        pTag.append(details);
+        $(list).append(details);
       })
     });
   }
-
-
 
 
   $('#load').on('click', () => {
