4f5980d0f7880ea3863c3b72e471b82338e33c9a
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-22 13:57:09 -0800

    can view an individual trips details.

--- index.js
+++ index.js
@@ -4,40 +4,35 @@ $(document).ready(() => {
       console.log('success!');
 
       response.forEach(function(trip){
-        let tripInfo = `<article data-id=${trip.id} data-name=${trip.name} data-continent=${trip.continent} data-weeks=${trip.weeks}>
+        let tripInfo = $(`<article data-id=${trip.id} data-name=${trip.name} data-continent=${trip.continent} data-weeks=${trip.weeks}>
           <ul class="details">
             <li>${trip.name}</li>
             <button class="trip-details">View Trip Details</button>
           </ul>
-        </article>`;
+        </article>`);
 
         $('#trips').append(tripInfo);
 
         // moving this inside of the first each loop so that we have access to the ID so we can pass it to the loadDetails function.
-        $('.trip-details').on('click',() => {
+        tripInfo.find('.trip-details').on('click', function() {
           console.log('WORKING?????');
           loadDetails(trip.id);
+
         });
 
-      // });
       const loadDetails = function loadDetails(id) {
         $.get(`https://trektravel.herokuapp.com/trips/${id}`, (response) => {
-          console.log('IT WORKED');
-
-          // response.forEach(function(info){
-            let addInfo = `<ul data-id=${response.id} data-name=${response.name} data-continent=${response.continent} data-about=${response.about} data-weeks=${response.weeks} data-category=${response.category} data-cost=${response.cost}>
-              <li>${response.name}</li>
-              <li>${response.continent}</li>
+          // console.log('IT WORKED');
+          // console.log(response);
+            let addInfo = `<li>${response.continent}</li>
               <li>${response.about}</li>
               <li>${response.weeks}</li>
               <li>${response.category}</li>
-              <li>${response.cost}</li>
-            </ul`;
+              <li>${response.cost}</li>`;
 
-            // let firstLi = $(this).closest('li');
-            $('.details').append(addInfo);
-            // $('.trip_details').remove();
-          // })
+            $(`[data-id="${id}"] .details`).append(addInfo);
+            $(`[data-id="${id}"] .trip-details`).remove();
+
         });
        }
      });
