076012477460bbff6c6d28b829bf34eb78951f96
Tanisha <tanishahampden@gmail.com>
2017-11-22 10:43:27 -0800

    add trip details to loadTrip functionality

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ $( document ).ready(function() {
         console.log('success!');
         console.log(response);
         // for (let i = 0; i < response.length; i++) {
-        //   // TODO: add link to li items
         //   $('ul').append(`<li><h3>${response[i]["name"]}</h3></li> continent : ${response[i]["continent"]}`)
         // } // end of for loop
 
         response.forEach(function(trip) {
-          let tripInfo = `<li><h3 data-id=${trip.id}><a href="https://trektravel.herokuapp.com/trips/${trip.id}">${trip.name}</a></h3><p>${trip.continent}</p></li>`
+          let tripInfo = `<li><h3 data-id=${trip.id}>${trip.name}</h3><p>${trip.continent}</p></li>`
           $('ul').append(tripInfo);
         }); // end of forEach
 
@@ -19,8 +18,19 @@ $( document ).ready(function() {
 
 
   loadTrip = function loadTrip(id) {
-    $.get('https://trektravel.herokuapp.com/trips/id',
+    $.get(`https://trektravel.herokuapp.com/trips/${id}`,
       response => {
+        let tripInfo = `
+        <h2> ${response.name} </h2>
+        <p> continent: ${response.continent} </p>
+        <p> about: ${response.about} </p>
+        <p> category: ${response.category} </p>
+        <p> weeks: ${response.weeks} </p>
+        <p> cost: ${response.cost} </p>`;
+
+        $('#trip').html(tripInfo);
+
+
         console.log('success!');
         console.log(response);
       })
