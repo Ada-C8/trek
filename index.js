588ff5d5e53a5c3adb00f8964ec4a3f24ec9ad3d
Amy Lee <amy.yj.lee@gmail.com>
2017-11-21 16:24:40 -0800

    wrote method findTrip to get details for oneTrip by tripID

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-
+const BASE_URL = 'https://trektravel.herokuapp.com/trips';
 const getTrips = () => {
-  $.get('https://trektravel.herokuapp.com/trips', (response) => {
+  $.get(BASE_URL, (response) => {
     console.log('success!');
     response.forEach((trip) => {
-      const tripInfo = `<li><strong id = 'trip-name'>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
+      const tripInfo = `<li data-id= ${trip.id}><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
       $('#trip-list ul').append(tripInfo);
     });
   }).fail(() => {
@@ -12,17 +12,38 @@ const getTrips = () => {
       console.log('Adenture awaits you!');
   });
 };
+const findTrip = (tripID) => {
+  $.get(`https://trektravel.herokuapp.com/trips/${tripID}`, (response) => {
+    console.log(response);
+  });
+};
 
 $(document).ready(() => {
-
+  // const BASE_URL = 'https://trektravel.herokuapp.com/trips';
+  // const getTrips = () => {
+  //   $.get(BASE_URL, (response) => {
+  //     console.log('success!');
+  //     response.forEach((trip) => {
+  //       const tripInfo = `<li data-id= ${trip.id}><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
+  //       $('#trip-list ul').append(tripInfo);
+  //     });
+  //   }).fail(() => {
+  //     $('#message').html('<h3> No trips found :( )</h3>');
+  //   }).always(() => {
+  //       console.log('Adenture awaits you!');
+  //   });
+  // };
+  // const findTrip = (tripID) => {
+  //   $.get(`https://trektravel.herokuapp.com/trips/${tripID}`, (response) => {
+  //     console.log(response);
+  //   });
+  // };
+// events
   $('#all-trips').on('click', () => {
     getTrips();
   });
-  $('#trip-list li').on('click', () => {
-    console.log(`you clicked me! I am a: ${this}`);
-    $.get(showTripURL, (response) => {
-      console.log('you showed the trip!');
-
-    })
+  $('#trip-list ul').on('click', 'li', function() {
+    const tripID = $(this).attr('data-id');
+    findTrip(tripID);
   });
 });
