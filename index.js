df8e8a77807a544604ed5c17641b4dcfaf9daa12
Sara C <srachandler@gmail.com>
2017-11-22 10:47:14 -0800

    can show individual trip info

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ $(document).ready(() => {
       (response) => {
         console.log(response);
         response.forEach(function(trek) {
-          let trekInfo = `<li><h3 data-id=${trek.id}> ${trek.name} </a></h3><p> ${trek.continent}, ${trek.weeks}</li>`
+          let trekInfo = `<li><h3 data-id=${trek.id}><a class="totrip"> ${trek.name} </a></h3><p> ${trek.continent}, ${trek.weeks}</li>`
 
           $('#trips ul').append(trekInfo);
         });
+
+      })
+      .fail(function(response){
+          console.log(response);
+          $('#fail').html('<p>Request was unsuccessful</p>')
+        })
+        .always(function(){
+          console.log('always even if we have success or failure');
+        });
+  });
+
+  $('#trips ul').on('click', 'h3', function(){
+    let tripID = $(this).attr('data-id');
+    $.get(`https://trektravel.herokuapp.com/trips/${tripID}`,
+      (response) => {
+        let tripInfo = `
+        <h2> ${response.name} </h2>
+        <p> Trip ID: ${response.id} </p>
+        <p> Continent: ${response.continent} </p>
+        <p> Category: ${response.category} </p>
+        <p> About: ${response.about} </p>
+        <p> Weeks: ${response.weeks} </p>
+        <p> Cost: ${response.cost} </p>`;
+
+        $('#trip').html(tripInfo);
 
       })
       .fail(function(response){
