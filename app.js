00b4b240ecd89d0d2e0d92e670732430d5bb5419
nkiru onwuneme <generalhrh@gmail.com>
2017-11-21 22:00:05 -0800

    used forEach loop to get list of all trips

--- app.js
+++ app.js
@@ -1,71 +1,52 @@
-$(function() {
-  $('#clickme').click(function() {
-       $.ajax({
-         method: 'GET',
-         url: 'https://trektravel.herokuapp.com/trips',
-         dataType: 'json',
-         success: function(data) {
-          let travelData = [];
-          console.log(data);
-          for(r in data) {
-            let key = r;
-            let val = data[r];
+$(document).ready(() => {
+  $('#clickme').on('click', function() {
 
-            travelData.push('<li id="' + key + '">' + val.id + '</li>');
-          }
-          console.log(travelData);
+    $.get('https://trektravel.herokuapp.com/trips',
+    response => {
+      console.log('success!');
+      //console.log(JSON.stringify(response));
+      response.forEach(function(trip) {
+        let tripInfo = `<li><h3 data-id=${trip.id}>  <a href='#'>${trip.name} </a></h3></li>`
+
+        $('#trips ul').append(tripInfo);
+
+      });
+    })
 
 
-          $('<ul/>', {
-             'class': 'interest-list',
-             html: travelData.join('')
-          }).appendTo('body');
-
-       },
-      statusCode: {
-         404: function() {
-           alert('There was a problem with the server.  Try again soon!');
-         }
-       }
-    });
   });
 });
+// Another option that works
 
-
-
-
-
-
-
-
-
-
-
-//Wave 1: Click button or link to show all trips
+// $(function() {
+//   $('#clickme').click(function() {
+//        $.ajax({
+//          method: 'GET',
+//          url: 'https://trektravel.herokuapp.com/trips',
+//          dataType: 'json',
+//          success: function(data) {
+//           let travelData = [];
+//           console.log(data);
+//           for(r in data) {
+//             let key = r;
+//             let val = data[r];
 //
-// const url = 'https://trektravel.herokuapp.com/trips'
+//             travelData.push('<li id="' + key + '">' + val.id + '</li>');
+//           }
+//           console.log(travelData);
 //
-// const trekData = {
-//   id;
-//   name;
-//   continent;
-//   weeks;
-// };
 //
-// $(function(){
-//     $.getJSON('https://trektravel.herokuapp.com/trips', function(travelData) {
-//         console.log(data);
+//           $('<ul/>', {
+//              'class': 'interest-list',
+//              html: travelData.join('')
+//           }).appendTo('body');
+//
+//        },
+//       statusCode: {
+//          404: function() {
+//            alert('There was a problem with the server.  Try again soon!');
+//          }
+//        }
 //     });
+//   });
 // });
-
-
-// Which URL do we want to 'get'?
-// let url = 'https://trektravel.herokuapp.com/trips';
-//
-// // What do we want to happen when we get our response?
-// let successCallback =  (response) => {
-//   console.log('success!');
-// };
-//
-//
-// $.getJSON(url, successCallback);
