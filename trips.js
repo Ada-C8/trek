190440db76680f7f4223c8dc0af1a99dcd073898
Canaan West <canaan.west@gmail.com>
2017-11-21 16:05:00 -0800

    show details page for a trip

--- trips.js
+++ trips.js
@@ -2,13 +2,34 @@
 $(document).ready(()=>{
   $('#all').on('click', () => {
     const url = 'https://trektravel.herokuapp.com/trips'
-    $.get(url, response=>{
+
+    $.get(url, response => {
+      $('#trips').empty();
       const allTrips = response;
       for (let i = 0; i < allTrips.length; i++){
-        let trip = `<h2>${allTrips[i].name}</h2>`
-        $('#trips').append($(trip).attr('id', `trip${i}`))
+        const trip = allTrips[i]
+        $('#trips').append($(`<h3>${trip.name}</h3>`).attr('id', `${trip.id}`));
+        console.log(trip);
       }
-
+    })
+    .fail(function() {
+      $('#trips').text('Sorry! Invalid Request.')
+    })
+    .always(function() {
+      console.log("this always happens!")
     })
   });
+
+  $('#trips').on('click', 'h3', function(){
+    const id = $(this).attr('id');
+    console.log(id)
+    const url2 = `https://trektravel.herokuapp.com/trips/${id}`
+    $.get(url2, response => {
+      const thisTrip = response
+      console.log(thisTrip);
+      $('#trips').empty()
+      const details = `<h2>${thisTrip.name}</h2> <ul><li>${thisTrip.continent}</li><li>${thisTrip.category}</li><li>${thisTrip.about}</li><li>$${thisTrip.cost}</li><li>${thisTrip.weeks}</li></ul>`
+      $('#trips').append(details)
+    })
+  })
 });
