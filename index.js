289a60471ed8bfa373438781987f08fdec3a5f33
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-22 14:09:25 -0800

    fixed indentation and refactored functions to be arrow functions

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ $(document).ready(() => {
     $.get('https://trektravel.herokuapp.com/trips/', (response) => {
       console.log('success!');
 
-      response.forEach(function(trip){
-        let tripInfo = $(`<article data-id=${trip.id} data-name=${trip.name} data-continent=${trip.continent} data-weeks=${trip.weeks}>
+      response.forEach((trip) => {
+        const tripInfo = $(`<article data-id=${trip.id} data-name=${trip.name} data-continent=${trip.continent} data-weeks=${trip.weeks}>
           <ul class="details">
             <li>${trip.name}</li>
             <button class="trip-details">View Trip Details</button>
@@ -14,28 +14,27 @@ $(document).ready(() => {
         $('#trips').append(tripInfo);
 
         // moving this inside of the first each loop so that we have access to the ID so we can pass it to the loadDetails function.
-        tripInfo.find('.trip-details').on('click', function() {
+        tripInfo.find('.trip-details').on('click', () => {
           console.log('WORKING?????');
           loadDetails(trip.id);
-
         });
 
-      const loadDetails = function loadDetails(id) {
-        $.get(`https://trektravel.herokuapp.com/trips/${id}`, (response) => {
+        const loadDetails = function loadDetails(id) {
+          $.get(`https://trektravel.herokuapp.com/trips/${id}`, (response) => {
           // console.log('IT WORKED');
           // console.log(response);
             let addInfo = `<li>${response.continent}</li>
               <li>${response.about}</li>
               <li>${response.weeks}</li>
               <li>${response.category}</li>
-              <li>${response.cost}</li>`;
+              <li>${response.cost}</li>
+              <button>Reserve Spot</button>`;
 
             $(`[data-id="${id}"] .details`).append(addInfo);
             $(`[data-id="${id}"] .trip-details`).remove();
-
-        });
-       }
-     });
+          });
+        };
+      });
     });
   }
 
@@ -44,9 +43,3 @@ $(document).ready(() => {
     loadTrips();
   });
 });
-
-// tried turning off the click function.
-// limiting the click functions. (effects all buttons).
-// tried creating a new function.
-// something to do with it loading on page.
-// does not work outside of the load trips function.
