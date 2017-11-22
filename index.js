f25c8ad01ea5395c7505e656db4c74ec9972c757
murog <guillermina.muro@gmail.com>
2017-11-22 10:28:34 -0800

    response now creates an article with the associated trip's id, on click, additional tables are shown for that trip with more information

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
+/* eslint-disable */
+
 $('#load-trips').click(function(event) {
   $.get('https://trektravel.herokuapp.com/trips',
   response => {
     console.log('success!');
     console.log(response);
     for (let i = 0; i < response.length; i++) {
-      $('#trips').append(`<tr><td>${response[i].id}</td>
-        <td>${response[i].name}</td>
-        <td>${response[i].continent}</td>
-        <td>${response[i].weeks}</td></tr>`);
-    };
-    })
-    .fail(function(){
-      console.log('failure');
-      $('#message').html('<p>Request failed no0o0</p>');
-    })
-    .always(function(){
-      console.log('always even if we have success or failure');
-    }); // Note that this is where the semi-colon ends up
-});
+      $('#trips').append(`<article id='trip-${response[i].id}'><h3>trip #${response[i].id}</h3></article>`);
+      $(`#trip-${response[i].id}`).click(function(event) {
+        $.get(`https://trektravel.herokuapp.com/trips/${response[i].id}`,
+        trip => {
+          $(`#trip-${trip.id}`).append(`<table><tr><td>ID: ${trip.id}</td>
+            <td>NAME: ${trip.name}</td>
+            <td>CONTINENT:${trip.continent}</td>
+            <td>COST: ${trip.cost}</td>
+            <td>CATEGORY: ${trip.category}</td>
+            <td>WEEKS: ${trip.weeks}</td></tr></table>
+            <table>
+            <tr><td>ABOUT: ${trip.about}</td></tr></table>`);
+        });
+        })
+
+      };
+    });
+  });
+    // .fail(function(){
+    //   console.log('failure');
+    //   $('#message').html('<p>Request failed no0o0</p>');
+    // })
+    // .always(function(){
+    //   console.log('always even if we have success or failure');
+    // }); // Note that this is where the semi-colon ends up
+// });
