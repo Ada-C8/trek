70eeb2a5f8f4ec9055d4a5ad0da24374ce6f0d3f
Gale Harrington <gale.harrington19@gmail.com>
2017-11-21 16:54:50 -0800

    Fix semicolons

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-/*eslint-disable*/
 $(document).ready(() => {
   $('.reveal').click(() => {
     $('.reveal').hide();
     $('body').append('<table class="trips"><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
-    $.get('https://trektravel.herokuapp.com/trips', (response) => {
-      response.forEach((trip) => {
+    $.get('https://trektravel.herokuapp.com/trips', (indexResponse) => {
+      indexResponse.forEach((trip) => {
         $('.trips').append(`<tr class="${trip.id}"><td class="${trip.id}">${trip.name}</td><td class="${trip.id}">${trip.continent}</td><td class="${trip.id}">${trip.weeks} Weeks</td></tr>`);
-        const url = `https://trektravel.herokuapp.com/trips/${trip.id}`
-        $.get(url, (response) => {
-          $(`tr.${trip.id}`).after(`<tr><td colspan=3>${response.about}</td></tr>`)
-        })
+        const url = `https://trektravel.herokuapp.com/trips/${trip.id}`;
+        $.get(url, (showResponse) => {
+          $(`tr.${trip.id}`).after(`<tr><td colspan=2>${showResponse.about}<p>Cost: $${showResponse.price}</p></td></tr>`);
+        });
       });
       // $('tr').click((e) => {
       //   const id = e.target.className;
