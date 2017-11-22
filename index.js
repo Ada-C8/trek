968fafcda29ce8c3a8a989c98063f3b0d1be9c6b
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-11-21 16:32:36 -0800

    basic trip info

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL = 'https://trektravel.herokuapp.com/trips';
+const URL = 'https://trektravel.herokuapp.com/trips/';
 
 $(document).ready(function(){
   let numTrips = 10;
@@ -7,20 +7,44 @@ $(document).ready(function(){
     $.get(URL, function(response) {
       console.log(response);
       for(let i = 0; i < response.length; i++) {
-        $('#trip-list').append('<li class="trip" id="'
-                               + response[i].id
-                               + '"><h3>'
-                               + response[i].name
-                               + '</h3></li>');
+        $('#trip-list').append(
+          '<li class="trip" id="'
+          + response[i].id
+          + '"><h3 class="trip-component">'
+          + response[i].name
+          + '</h3></li>'
+        );
       }
     });
 
     $('main > section').toggleClass('hidden');
   });
 
-  $('#trip-list').on('click', 'h3', function(e){
+  $('#trip-list').on('click', '.trip-component', function(e){
     let id = e.target.parentElement.id;
-    $('.trip').removeClass('show');
-    $('#' + id).addClass('show');
+    $('.about-trip').remove();
+    target = $('#' + id);
+    if (target.hasClass('show')) {
+      target.addClass('show');
+    } else {
+      $('.trip').removeClass('show');
+      target.addClass('show');
+      $.get(URL + id, function(response) {
+        console.log(response);
+        target.append(
+          '<div class="about-trip trip-component"><p><strong>continent:</strong> '
+          + response.continent
+          + '</p><p><strong>category: </strong> '
+          + response.category
+          + '</p><p><strong>length: </strong> '
+          + response.weeks + ' weeks'
+          + '</p><p><strong>cost: </strong> $'
+          + response.cost
+          + '</p><p><em> '
+          + response.about
+          + '</em></p></div>'
+        );
+      });
+    }
   });
 });
