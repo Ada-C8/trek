23e4a6ca89f9b13a5a1066da408def2b3f5d113e
Amy Cash <amymcash@gmail.com>
2017-11-22 11:27:40 -0800

    Showing details, but still showing all trips

--- trips.js
+++ trips.js
@@ -11,14 +11,35 @@ let loadTrips = function loadTrips() {
 
   $.get('https://trektravel.herokuapp.com/trips', (response) => {
     response.forEach(function(trip)  {
-      let tripInfo = `<li><h3 data-id=${trip.id}> ${trip.name} </a></h3></li>`
+      let tripInfo = `<li><button class="button"><h3 data-id=${trip.id}> ${trip.name} </a></h3></button></li>`
 
       $('#trips ul').append(tripInfo);
     }); //for each
 
-  });// get response
+  });// get success response
 
 } //loadtrips function
+
+let loadDetail = function loadDetail(id) {
+  $.get(`https://trektravel.herokuapp.com/trips/${id}`, (response) => {
+    let tripDetail = `
+      <h2> ${response.name} </h2>
+      <p> ${response.continent} </p>
+      <p> ${response.about} </p>
+      <p> ${response.weeks} </p>
+      <p> ${response.cost} </p> `;
+
+
+    $('.trip-details').html(tripDetail);
+  }); //get trip function
+
+} //trip detail function
+
+$('#trips ul').on('click', 'h3', function () {
+  let tripID = $(this).attr('data-id');
+  loadDetail(tripID);
+}) //
+
 
 $('#load').on('click', function() {
   loadTrips();
