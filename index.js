dc3f91757228dc536400d161d5c70059ae8fda84
Jan Edrozo <jedrozo@gmail.com>
2017-11-22 05:07:27 -0800

    Added foundation

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
+  // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR ALL TRIPS
+  const loadTrips = function loadTrips() {
+    $.get('https://trektravel.herokuapp.com/trips', (response) => {
+      response.forEach((trip) => {
+        const tripID = `Trip: ${trip.id}`;
+        const tripName = `Name: ${trip.name}`
+        console.log(trip)
+        $('#featured-content').append((tripID + tripName));
+      });
+    }).fail(() => {
+      console.log('Did not load successfully!');
+    });
+  };
+
+  // EVENTS
+  $('#all-trips').on('click', function() {
+    loadTrips();
+  });
 });
