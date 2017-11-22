6bb812b626321ad8fd91cfb2430180e8bcbc268d
Laura Robertson <mslaura.mae@gmail.com>
2017-11-22 12:14:27 -0800

    Trips details .html instead of append, form for reservaiton added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $.get(
       const { continent } = response[i];
       const { weeks } = response[i];
       $('#trips').append(`<li id='${id}'>
-        <h3>${name}</h3>
+        <h3 id='${id}'>${name}</h3>
         <p>Continent: ${continent}</p>
         <p>Duration: ${weeks} weeks</p>
       </li>`);
@@ -23,12 +23,38 @@ const singleTrip = (id) => {
   $.get(
     `https://trektravel.herokuapp.com/trips/${id}`,
     (response) => {
+      const { name } = response;
+      const { continent } = response;
+      const { weeks } = response;
       const { about } = response;
       const { cost } = response;
       const { category } = response;
-      $(`#${id}`).append(`<p>Cost: ${cost}</p>
-          <p>Category: ${category}</p>
-          <p>${about}</p>`);
+      $(`#${id}`).html(`<h3>${name}</h3>
+        <p>Continent: ${continent}</p>
+        <p>Duration: ${weeks} weeks</p>
+        <p>Cost: ${cost}</p>
+        <p>Category: ${category}</p>
+        <p>${about}</p>
+        <form action='https://trektravel.herokuapp.com/trips/${id}/reservations' method='post'>
+          <section>
+            <label>Name</label>
+            <input type='text' id='name' name='name' />
+          </section>
+
+          <section>
+            <label>Age</label>
+            <input type='text' id='age' name='age' />
+          </section>
+
+          <section>
+            <label>Email</label>
+            <input type='text' id='email' name='email' />
+          </section>
+
+          <section>
+            <button type='submit'>Reserve Trip</button>
+          </section>
+        </form>`);
     },
   )
     .fail(() => {
@@ -41,7 +67,22 @@ $(document).ready(() => {
     $('#trips').toggle();
   });
 
-  $('#trips').on('click', 'li', (event) => {
+  $('#trips').on('click', 'h3', function(event) {
     singleTrip(event.currentTarget.id);
+  });
+
+  $('form').submit( function(e) {
+    e.preventDefault();
+    const url = $(this).attr('action');
+    const formData = $(this).serialize();
+
+    $.post(url, formData, (response) => {
+      $('form').append('<h3>Trip Reserved</h3>');
+      console.log(response);
+    }).fail(() => {
+      $('form').append('<h3>Trip failed to reserve</h3>');
+    }).always(() => {
+      console.log('Making code');
+    });
   });
 });
