98b0f93fb666cbb6b28a5ea088f872460d8e28f8
Gale Harrington <gale.harrington19@gmail.com>
2017-11-22 13:49:23 -0800

    Replace old index.js with refactor.js

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 /*eslint-disable*/
 $(document).ready(() => {
-  $('.reveal').click(() => {
-    $('.reveal').hide();
-    $('body').append('<table class="trips"><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
-    $.get('https://trektravel.herokuapp.com/trips', (indexResponse) => {
+  const index = function index() {
+    const url = 'https://trektravel.herokuapp.com/trips';
+    $.get(url, (indexResponse) => {
+      $('table:first-child').replaceWith('<table class="trips"><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
       indexResponse.forEach((trip) => {
-        $('.trips').append(`<tr class="${trip.id} basic"><td class="${trip.id}">${trip.name}</td><td class="${trip.id}">${trip.continent}</td><td class="${trip.id}">${trip.weeks} Weeks</td></tr>`);
-        const url = `https://trektravel.herokuapp.com/trips/${trip.id}`;
-        $.get(url, (showResponse) => {
-          $(`tr.${trip.id}`).after(`<tr class="more"><td colspan=2><p>About Trip</p>${showResponse.about}<p>Category: ${showResponse.category}<p>Cost: $${showResponse.cost}</p></td></tr>`);
-          $('.more').hide();
-        });
-        $('tr.basic').click((e) => {
-            console.log($(e.target.parent));
-            console.log($(e.target.parent).next('.more'));
-            $(e.target.parent).next('.more').toggle();
-        });
+        $('.trips').append(`<tr class="${trip.id}"><td>${trip.name}</td><td>${trip.continent}</td><td>${trip.weeks} Weeks</td></tr>`);
       });
-      // $('tr').click((e) => {
-      //   const id = e.target.className;
-      //   const url = `https://trektravel.herokuapp.com/trips/${id}`
-      //
-      // });
     });
+  };
+  const show = function show(trip) {
+    const url = `https://trektravel.herokuapp.com/trips/${trip}`;
+    $.get(url, (showResponse) => {
+      $('.show-trip').replaceWith(`<div class="show-trip"><h2>${showResponse.name}</h2><h3>Price: $${showResponse.cost}</h3><h3>Trip Duration: ${showResponse.weeks} Weeks</h3><h3>Continent: ${showResponse.continent}</h3><h3>Category: ${showResponse.category}</h3><p>${showResponse.about}</p></div>`);
+    });
+  };
+  $('.index-button').click((e) => {
+    $(e.target).hide();
+    $('.show-trip').hide();
+    $('.index-trips').show();
+    $('.show-trip').replaceWith('<div class="show-trip"></div>');
+    index();
+  });
+  $('.index-trips').on('click', 'tr', (e) => {
+    const id = e.target.closest('tr').className;
+    $('.index-button').show();
+    $('.index-trips').hide();
+    show(id);
+    $('.show-trip').show();
   });
 });
