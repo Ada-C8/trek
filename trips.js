355031a34ff771ce6b66cc386c3ac6f17d547c93
Amy Cash <amymcash@gmail.com>
2017-11-22 14:57:08 -0800

    More work on show detail

--- trips.js
+++ trips.js
@@ -11,7 +11,7 @@ let loadTrips = function loadTrips() {
 
   $.get('https://trektravel.herokuapp.com/trips', (response) => {
     response.forEach(function(trip)  {
-      let tripInfo = `<li><button class="button"><h3 data-id=${trip.id}> ${trip.name} </a></h3></button></li>`
+      let tripInfo = `<li> <button class="button"> <h3 data-id=${trip.id}> ${trip.name} </a> </h3> </button> </li>`
 
       $('#trips ul').append(tripInfo);
     }); //for each
@@ -28,8 +28,12 @@ let loadDetail = function loadDetail(id) {
       <p> ${response.about} </p>
       <p> ${response.weeks} </p>
       <p> ${response.cost} </p> `;
-
-      $('li').after(tripDetail);
+console.log(id);
+console.log(typeof(id));
+console.log($(this));
+//recognizes
+//how to tell it to use th eid?
+      $('h3').first().after(tripDetail);
     // $('h3').after(tripDetail); //works
   }); //get trip function
 
@@ -57,48 +61,3 @@ $('#load').on('click', function() {
 //     // $('button').remove(); //will remove all buttons when a button is clicked
 //   });
 // });
-
-
-
-//
-// $.get(url, successCallback);
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-// let loadPets = afunction loadPets() {
-//   $.get('https://petdibs.herokuapp.com/pets',
-//     (response) => {
-//
-//       response.forEach(function(pet) {
-//         let petInfo = `<li><h3 data-id=${pet.id}>
-//         ${pet.name} </a></h3>
-//         <p> ${pet.age}, ${pet.breed}</li>`
-//
-//         $('#pets ul').append(petInfo);
-//       }); //gend tet request
-//
-//     }) //end load pets function
-//     .fail(function(response){
-//         console.log(response);
-//         $('#fail').html('<p>Request was unsuccessful</p>')
-//       })
-//       .always(function(){
-//         console.log('always even if we have success or failure');
-//       });
-// };
