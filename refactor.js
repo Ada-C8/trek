e6f6e46a80a190ded93ec2c1d79bb83250110f6b
Gale Harrington <gale.harrington19@gmail.com>
2017-11-22 13:47:26 -0800

    Refactoring: show div being replaced correctly

--- refactor.js
+++ refactor.js
@@ -1,34 +1,32 @@
-/*eslint-disable*/
+
 $(document).ready(() => {
-  $('.show-trip').hide();
-  let index = function index() {
+  const index = function index() {
     const url = 'https://trektravel.herokuapp.com/trips';
     $.get(url, (indexResponse) => {
       $('table:first-child').replaceWith('<table class="trips"><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
       indexResponse.forEach((trip) => {
         $('.trips').append(`<tr class="${trip.id}"><td>${trip.name}</td><td>${trip.continent}</td><td>${trip.weeks} Weeks</td></tr>`);
-      })
-    })
-  }
-  let show = function show(trip) {
+      });
+    });
+  };
+  const show = function show(trip) {
     const url = `https://trektravel.herokuapp.com/trips/${trip}`;
     $.get(url, (showResponse) => {
-      console.log(showResponse);
+      $('.show-trip').replaceWith(`<div class="show-trip"><h2>${showResponse.name}</h2><h3>Price: $${showResponse.cost}</h3><h3>Trip Duration: ${showResponse.weeks} Weeks</h3><h3>Continent: ${showResponse.continent}</h3><h3>Category: ${showResponse.category}</h3><p>${showResponse.about}</p></div>`);
     });
   };
   $('.index-button').click((e) => {
     $(e.target).hide();
     $('.show-trip').hide();
     $('.index-trips').show();
-    $('.show-button').show();
+    $('.show-trip').replaceWith('<div class="show-trip"></div>');
     index();
-  })
+  });
   $('.index-trips').on('click', 'tr', (e) => {
-    // $(e.target).hide();
     const id = e.target.closest('tr').className;
     $('.index-button').show();
     $('.index-trips').hide();
-    $('.show-trip').show();
     show(id);
-  })
+    $('.show-trip').show();
+  });
 });
