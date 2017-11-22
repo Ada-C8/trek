3185dd20f1e0a4a08be9fd5b858d01b4fac15c11
Victoria Sawchuk <vtsawchuk@gmail.com>
2017-11-21 16:14:09 -0800

    refactor js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const loadTrips = function loadTrips(response) {
+  $('#content').html('<div id="content"><ul id="list"></ul></div>');
+  for (let i = 0; i < response.length; i += 1) {
+    const trip = response[i];
+    const weeks = trip.weeks === 1 ? 'week' : 'weeks';
+    const tripHTML = `<li><h3><a class='trip' id='${trip.id}' href="#">${trip.name}</a></h3><ul><li>${trip.continent}</li><li>${trip.weeks} ${weeks}</li></ul></li>`;
+    $('#list').append(tripHTML);
+  }
+};
+
+const showTrip = function showTrip(response) {
+  const tripHTML = `<a id='reserve' href='#' trip_id='${response.id}'>Reserve</a><h4>Details</h4><ul><li>ID: ${response.id}</li><li>Continent: ${response.continent}</li><li>About: ${response.about}</li><li>Category: ${response.category}</li><li>Weeks: ${response.weeks}</li><li>Cost: ${response.cost}</li></ul>`;
+  $('#title').html(`${response.name}`);
+  $('#content').html(tripHTML);
+};
+
 const createForm = function createForm() {
   const formElements = ['Name', 'Email', 'Age'];
   const tripID = $(this).attr('trip_id');
@@ -10,29 +26,15 @@ const createForm = function createForm() {
 
 $(document).ready(() => {
   $('#load-trips').on('click', () => {
-    $.get(
-      'https://trektravel.herokuapp.com/trips',
-      (response) => {
-        $('#content').html('<div id="content"><ul id="list"></ul></div>');
-        for (let i = 0; i < response.length; i += 1) {
-          const trip = response[i];
-          const weeks = trip.weeks === 1 ? 'week' : 'weeks';
-          const tripHTML = `<li><h3><a class='trip' id='${trip.id}' href="#">${trip.name}</a></h3><ul><li>${trip.continent}</li><li>${trip.weeks} ${weeks}</li></ul></li>`;
-          $('#list').append(tripHTML);
-        }
-      },
-    );
+    const url = 'https://trektravel.herokuapp.com/trips';
+    $.get(url, (response) => { loadTrips(response); });
   });
   $('#display').on('click', '.trip', (event) => {
-    $.get(
-      `https://trektravel.herokuapp.com/trips/${event.target.id}`,
-      (response) => {
-        const tripHTML = `<a id='reserve' href='#' trip_id='${response.id}'>Reserve</a><h4>Details</h4><ul><li>ID: ${response.id}</li><li>Continent: ${response.continent}</li><li>About: ${response.about}</li><li>Category: ${response.category}</li><li>Weeks: ${response.weeks}</li><li>Cost: ${response.cost}</li></ul>`;
-        $('#title').html(`${response.name}`);
-        $('#content').html(tripHTML);
-        $('#reserve').on('click', createForm);
-      },
-    );
+    const url = `https://trektravel.herokuapp.com/trips/${event.target.id}`;
+    $.get(url, (response) => {
+      showTrip(response);
+      $('#reserve').on('click', createForm);
+    });
   });
   $('body').on('submit', 'form', (e) => {
     e.preventDefault();
