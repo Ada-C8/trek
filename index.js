b5faf66b014139ebcdb1249da48130e62c0864c9
Maria McGrew <mcgmar1@outlook.com>
2017-11-22 10:49:42 -0800

    added callback functions to response for tripsInfo

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ $(document).ready(() => {
           const tripsInfo = `<li><p>${trip.name}</p></li>`
           $('#trips ol').append(tripsInfo);
         });
-      });
-  };
+    }).fail(() => {
+      $('#fail').html('<p>Request was unsuccessful</p>');
+    }).always(() => {
+      console.log('always message here');
+    });
+
+  }; // end of loadTrips
 
   // const loadTrip = function loadTrip(id) {
   //   $.get(`https://trektravel.herokuapp.com/trips/${id}`,
-  // }
+  //     (response) => {
+  //       let tripInfo =
+  //       <
+  //     });
+  // }; // end of loadTrips
 
   $('#load').on('click', function() {
     loadTrips();
