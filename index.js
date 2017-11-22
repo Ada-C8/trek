8d725e2b71a1c135084e8cd4283a29639e5208c3
averikitsch <aekitsch@gmail.com>
2017-11-21 16:22:42 -0800

    trip show

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 
 
 const tripUrl = 'https://trektravel.herokuapp.com/trips';
-const clock = '<i class="fa fa-clock-o" aria-hidden="true"></i>';
-const plane = '<i class="fa fa-plane" aria-hidden="true"></i>';
+const clockIcon = '<i class="fa fa-clock-o" aria-hidden="true"></i>';
+const planeIcon = '<i class="fa fa-plane" aria-hidden="true"></i>';
+const moneyIcon = '<i class="fa fa-money" aria-hidden="true"></i>';
+const idIcon = '<i class="fa fa-folder-o" aria-hidden="true"></i>';
+const categoryIcon = '<i class="fa fa-photo" aria-hidden="true"></i>';
+
 const loadTrips = function loadTrips() {
   $.get(tripUrl, (response) => {
+    $('#trips').empty();
     response.forEach((trip) => {
       const name = `<h3>${trip.name}</h3>`;
-      const location = `<li>${plane} ${trip.continent}</li>`;
+      const location = `<li>${planeIcon} ${trip.continent}</li>`;
       const week = (trip.week > 1 ? 'weeks' : 'week');
-      const time = `<li>${clock} ${trip.weeks} ${week}</li>`;
-      $('#trips').append(`<div class="trip">${name}<ul>${location}${time}</ul></div>`);
+      const time = `<li>${clockIcon} ${trip.weeks} ${week}</li>`;
+      $('#trips').append(`<div class="trip" id="${trip.id}">${name}<ul>${location}${time}</ul></div>`);
     });
   }).fail(() => {
     $('#trips').html('<p>Oops.. Check back for more adventures soon!</p>');
@@ -19,7 +24,30 @@ const loadTrips = function loadTrips() {
   });
 };
 
+const openTrip = function openTrip(id) {
+  $.get(`${tripUrl}/${id}`, (trip) => {
+    const name = `<h2>${trip.name}</h2>`;
+    const id = `<li>${idIcon} ${trip.id}</li>`;
+    const location = `<li>${planeIcon} ${trip.continent}</li>`;
+    const week = (trip.week > 1 ? 'weeks' : 'week');
+    const time = `<li>${clockIcon} ${trip.weeks} ${week}</li>`;
+    const cost = `<li>${moneyIcon} $${trip.cost.toFixed(2)}</li>`;
+    const category = `<li>${categoryIcon} ${trip.category}</li>`;
+    const about = `<p>${trip.about}</p>`;
+    $('#trips').html(`${name}<ul>${id}${location}${time}${category}${cost}</ul>${about}`);
+  }).fail(() => {
+
+  }).always(() => {
+
+  });
+};
+
 $(document).ready(() => {
+  $('#trips').on('click', '.trip', function() {
+    console.log('you clicked a trip!');
+    openTrip(this.id);
+  });
+
   $('#load').on('click', () => {
     console.log('you clicked this');
     loadTrips();
