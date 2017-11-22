cdeaef4c43eb6610e633ab334810b492b602019e
Angela Wilson <wilsonange@gmail.com>
2017-11-22 15:26:40 -0800

    working on post reservation. functionality not complete. commiting to work on a branch for modifying the one trip html.

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
+const url = 'https://trektravel.herokuapp.com/trips'
+
+const successCallback = function(response) {
+  console.log("POST request was successful");
+  console.log(response);
+
+  let generatedHMTL = '<p>Everything went great,';
+  generatedHMTL += `and your reservation ${ response["name"] } has been added to the DB!</p>`;
+  $('#ajax-results').html(generatedHMTL);
+};
+
 $(document).ready(function() {
   const getTrips = function getTrips () {
-    let url = 'https://trektravel.herokuapp.com/trips'
+    // let url = 'https://trektravel.herokuapp.com/trips'
 
     $.get(url,
       function (response) {
-        // let trips = response
-
-        // let tripName = response[0]
-        // console.log(tripName);
+        $('#trips ul').html('')
         response.forEach((trip) => {
-          let tripName = trip['name']
+          // let tripName = trip['name']
           // console.log(tripName);
 
-          $('#trips ul').append(`<li><a href="/Users/angelawilson/test/ADA/week-16/trek/trip.html" data-id="${trip['id']}" >${tripName}</a></li>`)
+          $('#trips ul').append(`<li><a href="" data-id="${trip['id']}" >${trip.name}</a></li>`)
         })
 
       } // end function (response)
@@ -20,34 +28,70 @@ $(document).ready(function() {
   } // end getTrips()
 
   const trip = function trip (id) {
-    let url = 'https://trektravel.herokuapp.com/trips/1'
+    let url = 'https://trektravel.herokuapp.com/trips/' + `${id}`
+
 
     $.get(url, function(response) {
-      // console.log(response);
+      console.log('success');
+      console.log(response['name']);
 
       let tripInfo =
       `
       <h2 data-id=${response.id}> ${response.name} </h2>
-      <p> Continent: ${response.continent} </p>
       <p> Category: ${response.category} </p>
       <p> Duration: ${response.weeks} weeks </p>
       <p> Cost: $${response.cost} </p>
       <p> About: ${response.about} </p>
+
+
+
       `;
 
-      $('#trip').append(tripInfo)
+
+      $('#trip').html(tripInfo)
     }) // end $.get
 
   } // end trip(id)
+
   // $('#loadTrips').on('click', getTrips)
-  $('#loadTrips').on('click', getTrips)
-  // $('#trip').on('click', trip)
+  $('#load-trips').on('click', getTrips)
+
+
+  $('#load-trips').on('click', function(event){
+    $('#trips').show();
+    $('#trip').hide();
+  });
+
 
   // 'a' targets the anchor tag with the trip.id in it. this gives the id when console.logging
-  $('#trips ul').on('click', 'a', function(){
+  $('#trips ul').on('click', 'a', function(event){
+    event.preventDefault();
     let tripID = $(this).attr('data-id');
     console.log(tripID);
     trip(tripID);
+    $('#trip').show();
+    $('#trips').hide();
   });
 
+  $('#add-res').on('submit', function(event) {
+    event.preventDefault();
+    console.log('$(this)');
+    console.log(this);
+    console.log($(this).data('id'));
+
+    let formData = $('#add-res').serialize();
+
+    console.log(formData);
+    // console.log(`${$(this).data('id')}`);
+
+    let resUrl = 'https://trektravel.herokuapp.com/trips/1/reservations'
+    // + `${id}` + '/reservations'
+
+    $.post(resUrl, formData, successCallback).fail((response) => {
+      console.log('Reservation failed');
+    })
+  })
+
+
+// Form code for submitting a reservation.
 })
