6dbe768a1ddec952813ca190715f322e62676a33
IsabelDePapel <isuchanek@gmail.com>
2017-11-21 21:37:43 -0800

    add ability to toggle trip details (hide/show) by clicking on trip name

--- index.js
+++ index.js
@@ -1,57 +1,56 @@
 const baseUrl = 'https://trektravel.herokuapp.com/trips';
 
+const formatCost = function formatCost(num) {
+  return num.toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2,
+  });
+};
+
 $(document).ready(() => {
   // callbacks;
   const loadTrips = function loadTrips() {
     $.get(baseUrl, (response) => {
       response.forEach((trip) => {
-        // console.log(response);
-        // const tripName = `<a class="trip-name">${trip.name}</a>`;
         const tripInfo = `<li><a data-id="${trip.id}">${trip.name}</a>`;
 
-        // $('ul.trips').append(html);
         $('.trips > ul').append(tripInfo);
       });
     });
   };
 
   const loadTrip = function loadTrip(tripId) {
-    const url = `${baseUrl}/${tripId}`;
+    // check if already displayed
+    if (!$(`[data-id=${tripId}]`).hasClass('loaded')) {
+      const url = `${baseUrl}/${tripId}`;
 
-    $.get(url, (response) => {
-      let tripInfo = '<section class="trip">';
-      const headings = Object.keys(response);
+      $.get(url, (response) => {
+        let tripInfo = '<section class="trip loaded">';
+        const headings = Object.keys(response);
 
-      for (let i = 0; i < headings.length; i += 1) {
-        // skip name bc already displayed
-        if (headings[i] !== 'name') {
-          tripInfo += `<p>${headings[i]}: ${response[headings[i]]}</p>`;
+        for (let i = 0; i < headings.length; i += 1) {
+          // skip name bc already displayed
+          if (headings[i] !== 'name') {
+            if (headings[i] === 'cost') {
+              tripInfo += `<p>${headings[i]}: ${formatCost(response[headings[i]])}</p>`;
+            } else {
+              tripInfo += `<p>${headings[i]}: ${response[headings[i]]}</p>`;
+            }
+          }
         }
-      }
-      tripInfo += '</section>';
+        tripInfo += '</section>';
 
-      console.log(tripInfo);
-      // $('a').data(`id="${tripId}"`).after(tripInfo);
-      // $(`a[data-id="${tripId}"]`).after(tripInfo);
-      // $('a').data(`${tripId}`).after(tripInfo);
-      $(`[data-id=${tripId}]`).after(tripInfo);
-    });
+        console.log(tripInfo);
+        // $('a').data(`id="${tripId}"`).after(tripInfo);
+        // $(`a[data-id="${tripId}"]`).after(tripInfo);
+        // $('a').data(`id: ${tripId}`).after(tripInfo);
+        $(`[data-id=${tripId}]`).addClass('loaded');
+        $(`[data-id=${tripId}]`).after(tripInfo);
+      });
+    }
   };
-
-  // const loadDetails = function loadDetails(tripId) {
-  //   const url = `${baseUrl}/${tripId}`;
-  //
-  //   $.get(url, (response) => {
-  //     let html = '';
-  //     const headings = Object.keys(response);
-  //     for (let i = 2; i < headings.length; i += 1) { // skip id and name
-  //       html += `<p>${headings[i]}: ${response[headings[i]]}<p>`;
-  //     }
-  //
-  //     $(`#trip${tripId} > p`).after(html);
-  //     $(`#trip${tripId}`).addClass('loaded');
-  //   });
-  // };
 
   // events
   $('#load').on('click', () => {
@@ -60,41 +59,12 @@ $(document).ready(() => {
 
   $('.trips').on('click', 'a', function load() {
     const tripId = $(this).data('id');
-    console.log(tripId);
-    loadTrip(tripId);
-    // const tripId = $(this).next().text();
-    //
-    // // if details already loaded
-    // if ($(`#trip${tripId}`).hasClass('loaded')) {
-    //   $(`#trip${tripId}`).toggle();
-    //   // load details and show
-    // } else {
-    //   loadDetails(tripId);
-    //   $(`#trip${tripId}`).toggle();
-    // }
-    // // const html = `<p>${trip.id}</p><p>${trip.continent}</p><p>${trip.about}</p>`;
-    // // $(this).after(html);
-    // // console.log('some text');
-    // // console.log($(this));
-    // console.log(tripId);
-    // // showDetails(tripId);
+
+    // check if loaded
+    if ($(this).hasClass('loaded')) {
+      $(this).next().toggle();
+    } else {
+      loadTrip(tripId);
+    }
   });
-
-  // $('.trips').on('click', 'section', function toggleDetails() {
-  //   // const tripId = ($(this).next().text());
-  //   // // if details already loaded
-  //   // if ($(`#trip${tripId}`).hasClass('loaded')) {
-  //   //   $(`#trip${tripId}`).toggle();
-  //   //   // load details and show
-  //   // } else {
-  //   //   loadDetails(tripId);
-  //   //   $(`#trip${tripId}`).toggle();
-  //   // }
-  //   const tripId = $(this).next().text();
-  //   $(`#trip${tripId}`).toggle();
-  // });
-
-  // const showDetails = function showDetails(trip) {
-  //
-  // }
 });
