11f2517cdbf2947b389978b7098b1e570ab0bb12
Julia <julia.meier@gmail.com>
2017-11-21 17:23:54 -0800

    can view a single trip's details

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ $(document).ready(()=>{
 
       response.forEach(function(trip) {
         let tripInfo =
-        `<li>Trip #${trip.id} </li>
-        <li> Name: ${trip.name}</li>
+        `<li><h3 trip-id=${trip.id}>Name: ${trip.name}</li>
         <li> Continent: ${trip.continent}</li>`;
         $('#trips ul').append(tripInfo);
         console.log('success!');
@@ -25,10 +24,41 @@ $(document).ready(()=>{
         });
   };
 
+  // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR A SPECIFIC TRIP
+    let loadTrip = function loadTrip(id){
+      $.get(`https://trektravel.herokuapp.com/trips/${id}`,
+        (response) => {
+          console.log(response);
+          let indivTripInfo = `
+          <h2> ${response.name} </h2>
+          <p> Continent: ${response.continent} </p>
+          <p> Trip Duration: ${response.weeks} week(s)</p>
+          <p> Trip Category: ${response.category} </p>
+          <p> Cost: $${response.cost} </p>
+          <p> Description: ${response.about} </p>
+          `;
+
+          $('#trip').html(indivTripInfo);
+
+        })
+        .fail(function(response){
+            console.log(response);
+            $('#fail').html('<p>Request was unsuccessful</p>')
+          })
+          .always(function(){
+            console.log('always even if we have success or failure');
+          });
+    };
+
 //EVENTS
   $('#load').on('click', function(){
      loadTrips();
    });
+
+   $('#trips ul').on('click', 'h3', function(){
+  let tripID = $(this).attr('trip-id');
+  loadTrip(tripID);
+});
 
 
 });
