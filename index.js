a6c4d66eef2e657c7d8592b79afb36cc2bd36254
Tanisha <tanishahampden@gmail.com>
2017-11-22 10:48:06 -0800

    whitespace and comments

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ $( document ).ready(function() {
   loadTrips = function loadTrips() {
     $.get('https://trektravel.herokuapp.com/trips',
       response => {
-        console.log('success!');
-        console.log(response);
-        // for (let i = 0; i < response.length; i++) {
-        //   $('ul').append(`<li><h3>${response[i]["name"]}</h3></li> continent : ${response[i]["continent"]}`)
-        // } // end of for loop
-
         response.forEach(function(trip) {
           let tripInfo = `<li><h3 data-id=${trip.id}>${trip.name}</h3><p>${trip.continent}</p></li>`
           $('ul').append(tripInfo);
@@ -15,7 +9,6 @@ $( document ).ready(function() {
 
       }) // end of $.get
   }; // end of loadTrips()
-
 
   loadTrip = function loadTrip(id) {
     $.get(`https://trektravel.herokuapp.com/trips/${id}`,
@@ -31,9 +24,6 @@ $( document ).ready(function() {
 
         $('#trip').html(tripInfo);
 
-
-        console.log('success!');
-        console.log(response);
       })
   }; // end of loadTrip()
 
