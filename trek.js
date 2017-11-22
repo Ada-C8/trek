5d071657806280e209599d1020b85bf8394ca48f
Stef <seedsofthesesame@gmail.com>
2017-11-22 14:07:03 -0800

    Button call working for all trips, though append is causing the list to repeat.

--- trek.js
+++ trek.js
@@ -1,15 +1,13 @@
 $(document).ready(() => {
 
-  const allTrips = () => {
+  $('#load').click(function trips() {
     $.get('https://trektravel.herokuapp.com/trips', (response) => {
       response.forEach((place) => {
         const locations = `
         <h2> ${place.name} </h2>`;
         console.log(locations);
-        // console.log('success!');
         $('#locations').append(locations);
       });
     });
-  };
-  allTrips();
+  });
 });
