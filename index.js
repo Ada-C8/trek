adafb7a7a4e161c5e4c6d42011963c835f8b303c
Jan Edrozo <jedrozo@gmail.com>
2017-11-22 13:24:29 -0800

    W1: Organized trips in a table upon clicking button(All Trips)

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+/* eslint-disable */
 $(document).ready(() => {
 
   // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR ALL TRIPS
   const loadTrips = function loadTrips() {
     $.get('https://trektravel.herokuapp.com/trips', (response) => {
+      // console.log(response);
+      $('#trips-body').html('');
       response.forEach((trip) => {
-        const tripID = `Trip: ${trip.id}`;
-        const tripName = `Name: ${trip.name}`
-        console.log(trip)
-        $('#featured-content').append((tripID + tripName));
+        console.log(trip);
+        // create html components to format each trip
+        //const tripInfo = `Trip: ${trip.id} Name: ${trip.name}`;
+        const tableBody = `
+          <tr>
+            <td>${trip.id}</td>
+            <td>${trip.name}</td>
+            <td>${trip.continent}</td>
+            <td>${trip.weeks}</td>
+            <td><button id="trip-details"><em>View Trip</em></button></td>
+          </tr>`
+
+        //$('#featured-content').append(tripInfo);
+        $('#trips-body').append(tableBody);
+
       });
+
     }).fail(() => {
       console.log('Did not load successfully!');
     });
@@ -18,4 +33,6 @@ $(document).ready(() => {
   $('#all-trips').on('click', function() {
     loadTrips();
   });
+
+
 });
