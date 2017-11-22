11faef2e2ec6134d5419c29d20a0e0d4d1fca531
Tanisha <tanishahampden@gmail.com>
2017-11-22 09:59:20 -0800

    add named function expressions to loadTrips and load trip functionality

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 $( document ).ready(function() {
-  $.get('https://trektravel.herokuapp.com/trips',
-    response => {
-      console.log('success!');
-      console.log(response);
-      for (let i = 0; i < response.length; i++) {
-        // TODO: add link to li items
-        $('ul').append(`<li><h3>${response[i]["name"]}</h3></li> continent : ${response[i]["continent"]}`)
-      }
-    })
-});
+  loadTrips = function loadTrips() {
+    $.get('https://trektravel.herokuapp.com/trips',
+      response => {
+        console.log('success!');
+        console.log(response);
+        // for (let i = 0; i < response.length; i++) {
+        //   // TODO: add link to li items
+        //   $('ul').append(`<li><h3>${response[i]["name"]}</h3></li> continent : ${response[i]["continent"]}`)
+        // } // end of for loop
+
+        response.forEach(function(trip) {
+          let tripInfo = `<li><h3>${trip.id}> ${trip.name} </a></h3><p> ${pet.continent}</li>`
+          $('ul').append(tripInfo);
+        }); // end of forEach
+
+      }) // end of $.get
+  }; // end of loadTrips()
+
+
+
+
+  loadTrip = function loadTrip(id) {
+    $.get('https://trektravel.herokuapp.com/trips/id',
+      response => {
+        console.log('success!');
+        console.log(response);
+      })
+  };
+}); //end of document.ready
