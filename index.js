60f3dc20d5318ab6ea30dfec5ac1686318e35bde
Gale Harrington <gale.harrington19@gmail.com>
2017-11-21 16:33:58 -0800

    Can access individual trip info

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 /*eslint-disable*/
-
 $(document).ready(() => {
   $('.reveal').click(() => {
     $('.reveal').hide();
     $('body').append('<table class="trips"><tr><th>Name</th><th>Continent</th><th>Duration</th></tr></table>');
-    $.get('https://trektravel.herokuapp.com/trips', response => {
+    $.get('https://trektravel.herokuapp.com/trips', (response) => {
       response.forEach((trip) => {
-        $('.trips').append('<tr><td>' + trip.name + '</td><td>' + trip.continent + '</td><td>' + trip.weeks + '</td></tr>')
-      })
-    })
-  })
+        $('.trips').append(`<tr class="${trip.id}"><td class="${trip.id}">${trip.name}</td><td class="${trip.id}">${trip.continent}</td><td class="${trip.id}">${trip.weeks} Weeks</td></tr>`);
+      });
+      $('tr').click((e) => {
+        const id = e.target.className;
+        const url = `https://trektravel.herokuapp.com/trips/${id}`
+        $.get(url, (response) => {
+          console.log(response);
+          $(`tr.${id}`).after(`<tr><td>${response.about}<td></tr>`)
+        })
+      });
+    });
+  });
 });
