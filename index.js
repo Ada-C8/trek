6a989abe8b3e136e9c955ad46d62e24453fe0886
Andrea Chen <andreachen.ac@gmail.com>
2017-11-22 14:57:44 -0800

    Add buildIndividualTrip and createReservation

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const ALL_TRIPS_URL = 'https://trektravel.herokuapp.com/trips';
 
-// build individual trip data
-// build form
-
 // build all trips
 let buildAllTrips = function buildAllTrips() {
 
   const successResponse = function successResponse(response) {
-    $(".result").html("Success");
     response.forEach(function(trip) {
       let name = trip.name,
           continent = trip.continent,
@@ -26,17 +22,32 @@ let buildAllTrips = function buildAllTrips() {
     response => {
       successResponse(response);
     })
-  .fail(failResponse())
+  .fail(failResponse)
 };
 
-$(document).ready(() => {
-  // listen for all-trips
-  $('#all-trips').click(function() {
-    buildAllTrips()
-  }); //#all-trips
+// submit reservation
+let createReservation = function createReservation(event) {
+  event.preventDefault();
 
-  // Event listening for individual trip click
-  $('.list-trips').on('click', 'li', function(event) {
+  let resUrl = $('#add-res').attr('action');
+  let formData = $('#add-res').serialize();
+
+  const successResponse = function successResponse() {
+    console.log('Post successful');
+    $("#add-res").html("Successfully made reservation");
+  };
+
+  const failResponse = function failResponse() {
+    console.log('Post unsuccessful');
+    $(".result").html("Failed to make reservation");
+  };
+
+  $.post(resUrl, formData, successResponse()).fail(failResponse)
+  // });
+}; //submit res
+
+// build individual trip data
+let buildIndividualTrip = function buildIndividualTrip(event) {
     let tripId = $(this).attr('data-id');
     let tripUrl = ALL_TRIPS_URL + '/' + `${tripId}`;
     $.get(tripUrl, response => {
@@ -59,32 +70,21 @@ $(document).ready(() => {
         <input type="submit" value="Reserve trip"></input>
         </form>`;
         $(this).after(form).hide();
-
-        // listen for submit and post
-        $('#add-res').submit(function(event) {
-          event.preventDefault();
-
-          let resUrl = $('#add-res').attr('action');
-          let formData = $('#add-res').serialize();
-
-          const successResponse = function successResponse() {
-            console.log('Post successful');
-            $("#add-res").html("Successfully made reservation");
-          };
-
-          const failResponse = function failResponse() {
-            console.log('Post unsuccessful');
-            $(".result").html("Failed to make reservation");
-          };
-
-          $.post(resUrl, formData, successResponse()).fail(failResponse())
-          // });
-        }); //submit res
-
       }); //.one click to make res
 
     });
 
+}
 
-});
+$(document).ready(() => {
+  // listen for all-trips
+  $('#all-trips').click(function() {
+    buildAllTrips()
+  }); //#all-trips
+
+  // Event listening for individual trip click
+  $('.list-trips').on('click', 'li', buildIndividualTrip)
+
+  // listen for submit and post
+  $('.list-trips').on('submit', '#add-res', createReservation);
 }); //document.ready
