7f69e152450ff9dd867939cb8e7275d33009e1b5
Stef <seedsofthesesame@gmail.com>
2017-11-21 22:02:32 -0800

    Most of the workings complete to show all trips.

--- trek.js
+++ trek.js
@@ -1,41 +1,16 @@
-// Wave 1 - Click a button/link to show all trips
+$(document).ready(() => {
 
-$(document).ready( function() {
+  const allTrips = () => {
+    // const allTheTrips = ()
 
-  const allTrips =
-
-  seven_wonders.forEach((location) => {
-    console.log(location);
-
-    let address = location;
-
-    $.get(`https://trektravel.herokuapp.com/trips`,
-      (response) => {
-        let locations = `
-        <h2> ${response["results"][0]["formatted_address"]} </h2>
-        <p> Latitude: ${(response["results"][0]["geometry"]["location"]["lat"])} </p>
-        <p> Longitude: ${(response["results"][0]["geometry"]["location"]["lng"])} </p>`
-        console.log(locations)
-        console.log('success!');
-        console.log(response["results"][0]["geometry"]["location"]);
-        $('#locations').append(locations);
-      });
+    $.get('https://trektravel.herokuapp.com/trips', (response) => {
+      const locations = `
+      <h2> ${response[0].name} </h2>`;
+      console.log(locations);
+      console.log('success!');
+      $('#locations').append(locations);
+      response.forEach();
     });
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$(document).ready(function()
+  };
+  allTrips();
+});
