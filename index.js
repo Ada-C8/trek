b650a86e622e42e95e23c9765dd09ba771981177
Tanja Stroble <plusdeltah@gmail.com>
2017-11-21 20:23:03 -0800

    renders individual trip

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
   $('#allTrips').on('click', function() {
-    $(this).attr("disabled", true);
+    let $allTrips = $(this);
+
+    $allTrips.attr("disabled", true);
+
     $.ajax('https://trektravel.herokuapp.com/trips', {
       dataType: "json",
       success: function(response) {
-        document.getElementById('allTrips').removeAttribute('disabled');
+        $allTrips.removeAttr('disabled');
+
         let html = "<ul>";
         for(let i = 0; i < response.length; i++) {
-          html += `<li><a class="trip" href="#">${response[i].name}</a></li>`;
+          html += `<li><a data-id="${response[i].id}" class="trip" href="#">${response[i].name}</a></li>`;
         }
         html += "</ul>";
-
         $('#listOfTrips').html(html);
       },
       error: function(request, errorType, errorMessage) {
@@ -21,10 +24,24 @@ $(document).ready(function() {
 
   $('#listOfTrips').on('click', '.trip', function(event) {
     event.preventDefault();
-    $("#listOfTrips").click(function(){
-        $("li").toggle();
+    $tripId = $(this);
+    let tripUrl = `https://trektravel.herokuapp.com/trips/${$tripId.data("id")}`;
+    $.ajax(tripUrl, {
+      dataType: "json",
+      success: function(trip) {
+        $("#name").html(trip.name);
+        $("#destination").html(trip.destination);
+        $("#continent").html(trip.continent);
+        $("#about").html(trip.about);
+        $("#category").html(trip.category);
+        $("#weeks").html(trip.weeks);
+        $("#cost").html(trip.cost);
+      },
+      error: function(request, errorType, errorMessage) {
+        // TODO: handle errors here
+      }
     });
+    $("#tripSearch").hide();
+    $("#tripDetail").show();
   });
-
-
 });
