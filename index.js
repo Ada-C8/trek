cafb94ca5aff2746ea524cc61146504ac1fef364
Jan Edrozo <jedrozo@gmail.com>
2017-11-22 14:01:59 -0800

    Updated All Trips Table so header appears upon click

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 /* eslint-disable */
 $(document).ready(() => {
-
   // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR ALL TRIPS
   const loadTrips = function loadTrips() {
     $.get('https://trektravel.herokuapp.com/trips', (response) => {
       // console.log(response);
       $('#trips-body').html('');
+
+      const tableHeaders = `
+        <thead>
+          <tr>
+            <th width="100">ID</th>
+            <th width="200">Name</th>
+            <th width="200">Continent</th>
+            <th width="200">Weeks</th>
+            <th width="200">Trip Details</th>
+          </tr>
+        </thead>`;
+
+      $('#trips-table').append(tableHeaders);
+
       response.forEach((trip) => {
         console.log(trip);
         // create html components to format each trip
-        //const tripInfo = `Trip: ${trip.id} Name: ${trip.name}`;
         const tableBody = `
           <tr>
             <td>${trip.id}</td>
@@ -17,16 +29,19 @@ $(document).ready(() => {
             <td>${trip.continent}</td>
             <td>${trip.weeks}</td>
             <td><button id="trip-details"><em>View Trip</em></button></td>
-          </tr>`
+          </tr>`;
 
-        //$('#featured-content').append(tripInfo);
         $('#trips-body').append(tableBody);
-
       });
-
     }).fail(() => {
       console.log('Did not load successfully!');
+      $('#featured-content').html('<em>An error has occurred. All trips could not be loaded.</em>');
     });
+  };
+
+  // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR ALL TRIPS
+  const getTripDetails = function getTripDetails() {
+
   };
 
   // EVENTS
@@ -34,5 +49,7 @@ $(document).ready(() => {
     loadTrips();
   });
 
-
+  $('#trip-details').on('click', function() {
+    getTripDetails();
+  });
 });
