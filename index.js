e6e47dd633a52a5d42b0b1ccc1b0cf9ee3491164
Amy Lee <amy.yj.lee@gmail.com>
2017-11-22 10:44:44 -0800

    added basic commands to clear trip-detail information or the list of all trips depending on the buttons clicked using #.empty, page still unstyled

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const getTrips = () => {
   $.get(BASE_URL, (response) => {
     console.log('success!');
     response.forEach((trip) => {
-      const tripInfo = `<li data-id= ${trip.id}><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
+      const tripInfo = `<li data-id= ${trip.id} id = "trip-${trip.id}"><strong>${trip.name}</strong> - ${trip.weeks} week(s) in ${trip.continent} </li>`;
       $('#trip-list ul').append(tripInfo);
     });
   }).fail(() => {
@@ -12,9 +12,41 @@ const getTrips = () => {
       console.log('Adenture awaits you!');
   });
 };
+const hideDetails = () => {
+  console.log('you hid the details');
+  $('#trip-details').empty();
+};
+const reserveTrip = () => {
+  console.log('you are reserving this trip');
+}
 const findTrip = (tripID) => {
   $.get(`https://trektravel.herokuapp.com/trips/${tripID}`, (response) => {
     console.log(response);
+    const tripInfo = `
+    <section>
+      <h4>${response.name}<h/4>
+      <p>Trip ID: ${response.id}
+      </p>
+      <p>Trip Destination: ${response.continent}</p>
+      <p>Duration(in weeks): ${response.weeks}</p>
+      <p>Category: ${response.category}</p>
+      <p>Cost: $${response.cost}</p>
+      <p>About: ${response.about}</p>
+      <button id= "hide-details">Hide Details</button>
+      <button id= "reserve-trip">Reserve this Trip</button>
+    </section>
+    `;
+    // console.log(tripInfo);
+    // $(`#trip-${tripID}`).append(tripInfo);
+    $('#trip-details').append(tripInfo);
+    // $('#trip-details').toggle();
+    $('#hide-details').on('click', () => {
+      // $('#trip-details').toggle();
+      hideDetails();
+    });
+    $('#reserve-trip').on('click', () => {
+      reserveTrip();
+    });
   });
 };
 
@@ -40,10 +72,13 @@ $(document).ready(() => {
   // };
 // events
   $('#all-trips').on('click', () => {
+    // $('#trip-list').toggle();
     getTrips();
   });
   $('#trip-list ul').on('click', 'li', function() {
     const tripID = $(this).attr('data-id');
+    // $('#trip-details').toggle();
     findTrip(tripID);
+    $('#trip-list ul').empty();
   });
 });
