a38546e36c71456a198b56f8fa96514cfa886790
Jocelyn Gonzalez <jocegonz@gmail.com>
2017-11-22 16:56:26 -0800

    added event propagation for form submit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 /* eslint-disable */
 
 $(document).ready(() => {
-  $('form').submit(function(e) {
+  $('body').on('submit', 'form', function(e) {
     e.preventDefault();
     console.log(this);
-  })
+    const url = $(this).attr('action');
+    const formData = $(this).serialize();
+
+    $.post(url, formData, (response) => {
+      $("#message").html('<p>Spot reserved!</p>');
+      console.log(response);
+    }).fail(() => {
+      $("#message").html("<p>Unable to complete reservation</p>")
+    }).always(() => {
+      console.log("always always always here");
+    });
+  });
+
+
+
 
   let loadTrips = function loadTrips() {
     $.get('https://trektravel.herokuapp.com/trips',
@@ -14,52 +28,63 @@ $(document).ready(() => {
           let continent = trip.continent;
           let weeks = trip.weeks;
           let id = trip.id;
-          $('.panel').append(`
-              <button class='accordion2'>${name}</button>
-              <section class='panel2'>
-                <p>Continent: ${continent}</p>
-                <p>Duration: ${weeks} week(s)</p>
-                <form action='https://trektravel.herokuapp.com/trips/${id}/reservations' method='post'>
-                  <section>
-                    <label>Name</label>
-                    <input type="text" id="name" name="name"></input>
-                  </section>
-                  <section>
-                    <label>Age</label>
-                    <input type="text" id="age" name="age"></input>
-                  </section>
-                  <section>
-                    <label>Email</label>
-                    <input type="text" id="email" name="email"></input>
-                  </section>
-                  <section class='button'>
-                    <button class='reserve' type='submit'>Reserve Now</button>
-                  </section>
-                </form>
-              </section>
-            `);
+          let basicInfo = `
+            <section class='panel${id}'>
+              <h2 data-id=${id} class='accordion2'>${name}</h2>
+              <p>Continent: ${continent}</p>
+              <p>Duration: ${weeks} week(s)</p>
+            </section>`;
+            $('.panel').append(basicInfo);
         }); //loop ends here
       })
       .fail(function(response) {
         console.log(response);
-        $('#fail').html('<p>Request was unsuccessful</p>');
+        $('.fail').html('<p>Request was unsuccessful</p>');
         })
         .always(function() {
           console.log('Always always always');
         });
 
-      let tripDeets = function tripDeets(id){
-        $.get('https://trektravel.herokuapp.com/trips/${id}',
-          (response) => {
-            console.log('defining this');
-            console.log(this);
-          });
-          return this
-      }
-
   }; //end of loadTrips
-  //
-  // const url=
+
+  let loadTrip = function loadTrip(id) {
+    $.get(`https://trektravel.herokuapp.com/trips/${id}`,
+      (response) => {
+        let basicInfo = `
+            <p>ID: ${response.id}</p>
+            <p>About: ${response.about}</p>
+            <p>Category: ${response.category}</p>
+            <p>Duration: ${response.weeks} week(s)</p>
+            <p>Cost: ${response.cost}</p>
+            <section id='message'></section>
+            <form action='https://trektravel.herokuapp.com/trips/${id}/reservations' method='post'>
+              <section>
+                <label>Name</label>
+                <input type="text" id="name" name="name"></input>
+              </section>
+              <section>
+                <label>Age</label>
+                <input type="text" id="age" name="age"></input>
+              </section>
+              <section>
+                <label>Email</label>
+                <input type="text" id="email" name="email"></input>
+              </section>
+              <section class='button'>
+                <button class='reserve' type='submit'>Reserve Now</button>
+              </section>
+            </form>`;
+        $(`.panel${id}`).append(basicInfo);
+      }) //loop ends here
+      .fail(function(response) {
+        console.log(response);
+        $('.fail').html('<p>Request was unsuccessful</p>');
+        })
+        .always(function() {
+          console.log('Always always always');
+        });
+
+  }; //end of loadTrip
 
   let accordion = function accordion(){
     let acc = $('.accordion');
@@ -78,36 +103,51 @@ $(document).ready(() => {
     }
   }
 
-  let accordion2 = function accordion2(){
-    let acc = $(".accordion2");
-    console.log(acc);
-    let i;
-
-    for (i = 0; i < acc.length; i++) {
-
-      acc[i].onclick = function(){
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-      }
-    }
-  }
+  // let accordion2 = function accordion2(){
+  //   let acc = $(".accordion2");
+  //   console.log(acc);
+  //   let i;
+  //
+  //   for (i = 0; i < acc.length; i++) {
+  //
+  //     acc[i].onclick = function(){
+  //       this.classList.toggle("active");
+  //       let panel = this.nextElementSibling;
+  //
+  //       if (panel.style.display === "block") {
+  //           panel.style.display = "none";
+  //       } else {
+  //           panel.style.display = "block";
+  //       }
+  //     }
+  //   }
+  // }
 // *****************EVENTS
 
 
-  // // $('button').on('click', function() {
+  $('.panel').on('click', 'h2', function() {
+    let tripId = $(this).attr('data-id');
+    loadTrip(tripId);
+  });
+  $('.allTrips').on('click', function(){
     loadTrips();
-    accordion();
-    accordion2();
+    let val;
+    if ($(this).text() == 'All Trips') {
+      return 'Hide';
+    } else {
+      return 'All Trips';
+    }
+    // let linkText = $(this).text() == 'All Trips' ? 'Hide' : 'All Trips';
+    $('.tripsButton').html(linkText);
+  });
+
+    // accordion();
+
+    // accordion2();
+
   // //   $('.allTrips').toggle();
-  // //   linkText = $(this).text() == 'All Trips' ? 'Hide' : 'All Trips';
-  // //   $('.tripsButton').html(linkText);
-  // // });
+
+   // });
   // //
   // // $('.allTrips ul').on('click', 'p', function() {
   // //   console.log($(this).text());
