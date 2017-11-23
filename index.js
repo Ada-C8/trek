4e04b74fd229377447c3699f88b3d1dab4eb54fa
HelloMarisaMorris <hellomarisanmorris@gmail.com>
2017-11-22 16:56:26 -0800

    wave three complete. no styling

--- index.js
+++ index.js
@@ -28,18 +28,48 @@ $(document).ready(() => {
               <li>${response.weeks}</li>
               <li>${response.category}</li>
               <li>${response.cost}</li>
-              <button>Reserve Spot</button>`;
+
+              <form action="https://trektravel.herokuapp.com/trips/${id}/reservations" method="post">
+                <section>
+                  <label>Name</label>
+                  <input type="text" id="name" name="name"></input>
+                </section>
+
+                <section>
+                  <label>Email</label>
+                  <input type="text" id="email" name="email"></input>
+                </section>
+
+                <section class="button">
+                  <button type="submit">Reserve Spot</button>
+                </section>
+              </form>`;
 
             $(`[data-id="${id}"] .details`).append(addInfo);
             $(`[data-id="${id}"] .trip-details`).remove();
           });
         };
       });
+    }).fail(function(){
+      console.log('FAIL');
     });
   }
 
   $('#load').on('click', () => {
     $('#trips').empty();
     loadTrips();
+  });
+
+  $('body').on('submit', 'form', function(e) {
+    e.preventDefault();
+
+    const url = $(this).attr('action');
+    const formData = $(this).serialize();
+
+    $.post(url, formData, () => {
+      $('#message').html('<p> Reservation added! </p>');
+    }).fail(() => {
+      $('#message').html('<p> Adding Reservation failed! </p>');
+    });
   });
 });
