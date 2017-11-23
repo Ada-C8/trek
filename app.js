5e94c53d6cc342a5fb7a9c80c8a823c34e469a6c
nkiru onwuneme <generalhrh@gmail.com>
2017-11-22 22:17:36 -0800

    dynamically add rows to table

--- app.js
+++ app.js
@@ -5,12 +5,35 @@ $(document).ready(() => {
     response => {
       console.log('success!');
       //console.log(JSON.stringify(response));
+
+      let tableRows = '';
       response.forEach(function(trip) {
-        let tripInfo = `<li><h3 data-id=${trip.id}>  <a href='#'>${trip.name} </a></h3></li>`
+        tableRows += `<tr data-id=${trip.id}>`
+        for (let attr in trip) {
+          tableRows += '<td>' + trip[attr] + '</td>'
+        }
+        tableRows += '</tr>'
+      })
 
-        $('#trips ul').append(tripInfo);
+      // response.forEach(function(trip) {
+      //   let tripInfo = `<tr><h3 data-id=${trip.id}>  <a href='#'>${trip.name} </a></h3>
+      //
+      //   </li>`
 
-      });
+      $('#trips table').show().append(tableRows);
+        // $("#trips ul").append(
+        // "<table><tr><td>My column 1, row 1</td>" +
+        // "<td>My column 2, row 2</td></tr>" +
+        // "<tr><td>My column 1, row 2</td>" +
+        // "<td>My column 2, row 2</td></tr></table>");
+        // });
+
+
+
+        // let tripInfo = `<li><h3 data-id=${trip.id}>  <a href='#'>${trip.name} </a></h3></li>`
+        // $('#trips ul').append(tripInfo);
+
+      // });
     })
     .fail(function(response) {
       //{"readyState":0,"status":0,"statusText":"error"}
@@ -23,40 +46,93 @@ $(document).ready(() => {
     });
 
     // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR A SPECIFIC TRIP
-    let loadTrip = function loadTrip(id){
+    let loadTrip = function(id) {
+      $('#trip').html('');
       $.get(`https://trektravel.herokuapp.com/trips/${id}`,
         (response) => {
-          console.log('this is' + response);
+          // console.log('this is' + response[name]);  //why is this returning object object
           let tripInfo = `
-          <h2> ${response.name} </h2>
+          <h2> ${response[name]} </h2>
           <p> Id: ${response.id} </p>
           <p> Continent: ${response.continent} </p>
           <p> About: ${response.about} </p>
           <p> Category: ${response.category} </p>
           <p> Weeks: ${response.weeks} </p>
-          <p> Costs: ${response.cost} </p>`;
+          <p> Costs: ${response.cost} </p>
+          <form id="add-name-form" action="https://trektravel.herokuapp.com/trips/${id}/reservations">
+          <label for="name">Name:</label>
+          <input type="text" name="name"></input>
+
+          <label for="age">Age:</label>
+          <input type="number" age="age"></input>
+
+          <label for="email">Email:</label>
+          <input type="text" email="email"></input>
+
+          <input type="submit" value="Reserve Spot"></input>
+          </form>`;
 
           $('#trip').html(tripInfo);
 
         })
         .fail(function(response){
-            console.log(response);
-            $('#fail').html('<p>Request was unsuccessful</p>')
-          })
-          .always(function(){
-            console.log('always even if we have success or failure');
-          });
+          console.log(response);
+          $('#fail').html('<p>Request was unsuccessful</p>');
+        })
+        .always(function(){
+          console.log('always even if we have success or failure');
+        });
     };
-
-
-    // EVENTS
-    $('#trips ul').on('click', 'h3', function(){
+    $('#trips table').on('click', 'tr',  function(){
       let tripID = $(this).attr('data-id');
+      console.log(tripID);
+      $('#trip').show();
       loadTrip(tripID);
+      // console.log(loadTrip);
+    });
+  });
+
+    const successCallback = function(response) {
+      console.log("POST request was successful");
+      console.log(response);
+
+      let generatedHTML = '<p>Everything went great,';
+      generatedHTML += `and your ${ response["name"] } has been added to the DB!</p>`;
+      console.log("we're here" + generatedHTML);
+      $('#ajax-results').html(generatedHTML);
+    }
+    // FUNCTION TO RESERVE A SPOT TO A SINGLE TRIP
+
+    $('#trip').on('submit', "#add-name-form", function(event) {
+      event.preventDefault();
+
+      let formData = $('#add-name-form').serialize();
+      console.log(formData);
+
+      const url = $('#add-name-form').attr('action');
+
+      $.post(url, formData, successCallback).fail((response) => {
+        console.log("Unable to post");
+      });
     });
 
-  });
-});
+
+  //   // EVENTS
+  // why is this out of scope here?
+  // $('#trips ul').on('click', 'h3', function(){
+  //   let tripID = $(this).attr('data-id');
+  //   console.log(tripID);
+  //   loadTrip(tripID);
+  //   // console.log(loadTrip);
+  // });
+  $("#clickme").click(function(){
+        $("#trip").hide();
+    });
+
+}); //end of document ready
+
+
+
 // Another option that works
 
 // $(function() {
