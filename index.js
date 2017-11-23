028bee5da1281fe0c20d351926f0e1ffb2412389
Jan Edrozo <jedrozo@gmail.com>
2017-11-22 17:26:37 -0800

    W2: Trip Details functionality added to button (View Trip)

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// /* eslint-disable */
+/* eslint-disable */
 $(document).ready(() => {
   // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR ALL TRIPS
   const loadTrips = function loadTrips() {
@@ -27,7 +27,7 @@ $(document).ready(() => {
             <td>${trip.name}</td>
             <td>${trip.continent}</td>
             <td>${trip.weeks}</td>
-            <td><button id="trip-details"><em>View Trip</em></button></td>
+            <td><button id="trip-details" data-id="${trip.id}"><em>View Trip</em></button></td>
           </tr>`;
 
         $('#trips-body').append(tableBody);
@@ -39,17 +39,49 @@ $(document).ready(() => {
   };
 
   // FUNCTION FOR AJAX REQUEST AND RESPONSE FOR TRIP DETAILS
-  const getTripDetails = function getTripDetails() {
+  const getTripDetails = function getTripDetails(id) {
+    // Clear existing table
     $('#trips-head').html('');
     $('#trips-body').html('');
 
-    $.get('https://trektravel.herokuapp.com/trips', (response) => {
-      // console.log(response);
-      // response.forEach((trip) => {
-      // });
+    const singleTripURL = `https://trektravel.herokuapp.com/trips/${id}`;
+
+    $.get(singleTripURL, (response) => {
+      console.log(response);
+      const singleTripTable = `
+      <h3>Trip Details</h3>
+      <table id="atrip-table">
+        <thead id="atrip-head">
+          <tr>
+            <th width="100">ID</th>
+            <th width="200">Name</th>
+            <th width="150">Continent</th>
+            <th width="800">About</th>
+            <th width="150">Category</th>
+            <th width="100">Weeks</th>
+            <th width="100">Cost</th>
+            <th width="100">Reserve</th>
+          </tr>
+        </thead>
+        <tbody id="atrip-body">
+          <tr>
+            <td class="atrip-cell">${response.id}</td>
+            <td class="atrip-cell">${response.name}</td>
+            <td class="atrip-cell">${response.continent}</td>
+            <td class="atrip-cell">${response.about}</td>
+            <td class="atrip-cell">${response.category}</td>
+            <td class="atrip-cell">${response.weeks}</td>
+            <td class="atrip-cell">${response.cost}</td>
+          </tr>
+        </tbody>
+      </table>
+      `;
+
+      $('#single-trip-info').html(singleTripTable);
+
     }).fail(() => {
       console.log('Did not load successfully!');
-      $('#featured-content').html('<em>An error has occurred. The trip details could not be loaded.</em>');
+      $('#featured-content').html('<p><em>An error has occurred. The trip details could not be loaded.</em></p>');
     });
   };
 
@@ -60,6 +92,7 @@ $(document).ready(() => {
 
   $('#featured-content').on('click', '#trip-details', function() {
     //console.log('Button trip details clicked');
-    getTripDetails();
+    let tripID = $(this).attr('data-id');
+    getTripDetails(tripID);
   });
 });
