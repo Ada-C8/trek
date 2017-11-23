d8106af7f2ee251348f0c310e652331f1db7863b
kee nam <nam.keeyeon@gmail.com>
2017-11-22 21:23:43 -0800

    In process of adding reservation function -- added modal and working on post logic

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ $(document).ready(function() {
     url = "https://trektravel.herokuapp.com/trips";
     $.get(`${url}`, function(data) {
       for (let trip of data) {
-        let tripInfo = $(
-          '<tr id="trip' + trip.id + '">'
-          + '<td>' + '<a class="trip-title" data-id =' + trip.id + '>' + trip.name + '</a>' + '</td>'
-          + '<td>' + trip.continent + '</td>'
-          + '<td>' + trip.weeks + '</td>'
-          + '</tr>'
-        );
-        $('#trip-list').append(tripInfo);
+        if (trip.continent != null) {
+          let tripInfo = $(
+            '<tr id="trip' + trip.id + '">'
+            + '<td class="trip-name">' + '<a class="trip-title" data-id =' + trip.id + '>' + trip.name + '</a>' + '</td>'
+            + '<td class="trip-cont">' + trip.continent + '</td>'
+            + '<td class="trip-week">' + trip.weeks + '</td>'
+            + '</tr>'
+          );
+          $('#trip-list').append(tripInfo);
+        }
       }
     });
   };
@@ -23,7 +25,7 @@ $(document).ready(function() {
       let tripInfo = $(
         '<tr class="trip-deets">'
         + '<td colspan="3">'
-        + '<table>'
+        + '<table class="deet-table">'
         + '<tr>'
         + '<td>' + '<i class="fa fa-tags"></i> Category: ' + data.category + '</td>'
         + '<td>' + '<i class="fa fa-usd"></i> Cost: ' + data.cost + '</td>'
@@ -31,30 +33,56 @@ $(document).ready(function() {
         + '<tr>'
         + '<td class="about-text" colspan="2">' + data.about + '</td>'
         + '</tr>'
+        + '<tr>'
+        + '<td class="about-text" colspan="2"><button id="reserve-go" class="btn btn-warning">Reserve Trip</button>' + '</td>'
+        + '</tr>'
         + '</table>'
         + '</td>'
         + '</tr>'
       );
+      tripInfo.fadeIn('slow', function() {
+        $(`tr#trip${data.id}`).after(tripInfo);
+      })
 
-      $(`tr#trip${data.id}`).after(tripInfo);
+      // $(`tr#trip${data.id}`).after(tripInfo);
     });
   };
 
   let details_exist = false;
+  let current_trip_id;
 
   $('tbody#trip-list').on('click', 'a', function() {
     let tripID = $(this).attr('data-id');
-    if (details_exist == true) {
+
+    console.log(current_trip_id);
+
+    if (current_trip_id != tripID) {
+      $('tr[id^=trip]').css('background', 'none');
       $('table.table').find('tr.trip-deets').each(function() {
         $(this).remove();
       });
       details_exist = false;
+      current_trip_id = null;
+
+      loadTrip(tripID);
+      $(`tr#trip${tripID}`).css('background', '#777');
+      details_exist = true;
+      current_trip_id = tripID;
+
+      console.log(current_trip_id);
+    } else {
+      $('tr[id^=trip]').css('background', 'none');
+      $('table.table').find('tr.trip-deets').each(function() {
+        $(this).remove();
+      });
+      details_exist = false;
+      current_trip_id = null;
     }
-    loadTrip(tripID);
-    details_exist = true;
   });
 
-  $('#all-trips').on('click', function(event) {
+  console.log(current_trip_id);
+
+  $('#all-trips').on('click', function() {
     let imageHeight = $('section.hero-image').css('height');
     if (imageHeight == '780px') {
       $('section.hero-image').css('height', '100px');
@@ -64,7 +92,6 @@ $(document).ready(function() {
 
       $('h1.title').removeClass('col-12').addClass('col');
       $('div.button-wrapper').removeClass('col-12').addClass('col');
-
     }
     else {
       $('section.hero-image').css('height', '780px');
@@ -78,4 +105,67 @@ $(document).ready(function() {
     $('.table').toggle('slow');
     loadTrips();
   });
+
+  $('#trip-list').on('click', '#reserve-go', function() {
+    $('div#reserve-modal').show('slow');
+  });
+  $('i.modal-close').on('click', function() {
+    $('div#reserve-modal').hide('slow');
+  })
+
+  // $('#name-sort').on('click', function() {
+  //   if ($('tbody#trip-list tr:first-of-type').text()[0] != 'A') {
+  //     sortTable('asc');
+  //   } else {
+  //     sortTable('desc');
+  //   }
+  // });
+  //
+  // let sortTable = function sortTable(direction) {
+  //   let rows = $('tbody#trip-list tr').get();
+  //
+  //   rows.sort(function(a, b) {
+  //     let A = $(a).children('td').eq(0).text().toUpperCase();
+  //     let B = $(b).children('td').eq(0).text().toUpperCase();
+  //
+  //     if(direction == 'asc') {
+  //       return (A > B) ? 1 : 0;
+  //     } else {
+  //       return (A > B) ? 0 : 1;
+  //     }
+  //   });
+  //
+  //   $.each(rows, function(index, row) {
+  //     $('.trip-table').children('tbody').append(row);
+  //   });
+  // };
+
+
+    let reserve_url = `https://trektravel.herokuapp.com/trips/${current_trip_id}/reservations`;
+
+    let successCallback = function(response) {
+    console.log("POST request was successful");
+    console.log(response);
+
+    // let generatedHMTL = '<p>Everything went great,';
+    // generatedHMTL += `and your pet ${ response["name"] } has been added to the DB!</p>`;
+    // $('#ajax-results').html(generatedHMTL);
+  };
+
+  $('#reserve-form').on('submit', function(event) {
+    console.log(reserve_url);
+    event.preventDefault();
+
+    let formData = $('#reserve-form').serialize();
+    console.log(formData);
+
+    console.log(reserve_url);
+
+    $.post(reserve_url, formData, successCallback).fail((response) => {
+      console.log("Didn't go so hot");
+    });
+  });
+
+
+
 });
