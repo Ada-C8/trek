d686d6af0140c7a8136ee2fb7f5f17fa2eeb2424
Angela Wilson <wilsonange@gmail.com>
2017-11-23 09:41:37 -0800

    reservations post correctly.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const successCallback = function(response) {
   console.log(response);
 
   let generatedHMTL = '<p>Everything went great,';
-  generatedHMTL += `and your reservation ${ response["name"] } has been added to the DB!</p>`;
+  generatedHMTL += ` and your reservation ${ response["name"] } has been added to the DB!</p>`;
   $('#ajax-results').html(generatedHMTL);
 };
 
@@ -30,7 +30,9 @@ $(document).ready(function() {
     });
   } // end getTrips()
 
+  const tripId = ''
   const trip = function trip (id) {
+
     let url = 'https://trektravel.herokuapp.com/trips/' + `${id}`
 
 
@@ -38,8 +40,9 @@ $(document).ready(function() {
       console.log('success');
       console.log(response['name']);
 
-      let tripInfo =
-      `
+      // tripId = `${response.id}`
+
+      let tripInfo = `
       <h2 data-id=${response.id}> ${response.name} </h2>
       `;
 
@@ -47,10 +50,31 @@ $(document).ready(function() {
       $('#name').append(tripInfo)
       $('#category').append(response.category)
       $('#duration').append(response.duration)
-      $('#cost').append(response.cost)
+      $('#cost').append((response.cost).toFixed(2))
       $('#about').append(response.about)
-    }) // end $.get
 
+      // I am not sure if putting the reservation code with in the scope of the $.get trip is best practice. It is here because this was the simplest way to access the trip's id. I tried using the .data()
+      $('#add-res').on('submit', function(event) {
+        event.preventDefault();
+
+
+        let formData = $('#add-res').serialize();
+
+        console.log(formData);
+        // console.log(`${$(this).data('id')}`);
+
+        let resUrl = 'https://trektravel.herokuapp.com/trips/' + `${response.id}` + '/reservations'
+        console.log(resUrl);
+
+        $.post(resUrl, formData, successCallback).fail((response) => {
+          console.log('Reservation failed');
+        })
+      })
+    }) // end $.get
+    .fail(function(response){
+      console.log(response);
+      $('#fail').html('<p>Request was unsuccessful</p>');
+    });
   } // end trip(id)
 
   // $('#loadTrips').on('click', getTrips)
@@ -64,6 +88,7 @@ $(document).ready(function() {
 
 
   // 'a' targets the anchor tag with the trip.id in it. this gives the id when console.logging
+
   $('#trips ul').on('click', 'a', function(event){
     event.preventDefault();
     let tripID = $(this).attr('data-id');
@@ -73,24 +98,6 @@ $(document).ready(function() {
     $('#trips').hide();
   });
 
-  $('#add-res').on('submit', function(event) {
-    event.preventDefault();
-    console.log('$(this)');
-    console.log(this);
-    console.log($(this).data('id'));
-
-    let formData = $('#add-res').serialize();
-
-    console.log(formData);
-    // console.log(`${$(this).data('id')}`);
-
-    let resUrl = 'https://trektravel.herokuapp.com/trips/1/reservations'
-    // + `${id}` + '/reservations'
-
-    $.post(resUrl, formData, successCallback).fail((response) => {
-      console.log('Reservation failed');
-    })
-  })
 
 
 // Form code for submitting a reservation.
