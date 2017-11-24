97d7a230db5bd0df22618dd9fd8a90702265714b
nkiru onwuneme <generalhrh@gmail.com>
2017-11-23 17:35:44 -0800

    added image slider

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//hide rows not to be seen
+
 $(document).ready(() => {
   $('#search-trips').on('click', function() {
 
@@ -129,8 +131,115 @@ $(document).ready(() => {
         $("#trip").hide();
     });
 
+  //   function slideSwitch() {
+  //     let $active = $('#slideshow IMG.active');
+  //
+  //     if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+  //
+  //     let $next =  $active.next().length ? $active.next()
+  //         : $('#slideshow IMG:first');
+  //
+  //     $active.addClass('last-active');
+  //
+  //     $next.css({opacity: 0.0})
+  //         .addClass('active')
+  //         .animate({opacity: 1.0}, 1000, function() {
+  //             $active.removeClass('active last-active');
+  //         });
+  // }
+  //
+  // $(function() {
+  //     setInterval( "slideSwitch()", 5000 );
+  // });
+
 }); //end of document ready
 
+  function slideSwitch() {
+    let $active = $('#slideshow IMG.active');
+
+    if ( $active.length == 0 ) $active = $('#slideshow IMG:last');
+
+    let $next =  $active.next().length ? $active.next()
+        : $('#slideshow IMG:first');
+
+    $active.addClass('last-active');
+
+    $next.css({opacity: 0.0})
+        .addClass('active')
+        .animate({opacity: 1.0}, 1000, function() {
+            $active.removeClass('active last-active');
+        });
+}
+
+$(function() {
+    setInterval( "slideSwitch()", 5000 );
+});
+
+// $('.slider').each(function() {
+//   let $this = $this;
+//   let $group = $this.find('.slide-group');
+//   let $slides = $this.find('.slide');
+//   let buttonArray = [];
+//   let currentIndex = 0;
+//   let timeout;
+//
+//   function move(newIndex) {
+//     let animateLeft
+//     let slideLeft;
+//
+//     advance();
+//
+//     if ($group.is(':animated') || currentIndex === newIndex) {
+//       return;
+//     }
+//
+//     buttonArray[currentIndex].removeClass('active');
+//     buttonArray[newIndex].addClass('active');
+//
+//     if (newIndex > currentIndex) {
+//       slideLeft = '100%';
+//       animateLeft = '-100%';
+//     } else {
+//       slideLeft = '-100%';
+//       animateLeft = '100%';
+//     }
+//
+//     $slides.eq(newIndex).css( {left: slideLeft, display: 'block'} );
+//     $group.animate( {left: animateLeft}, function() {
+//       $slides.eq(currentIndex).css( {display: 'none'} );
+//       $slides.eq(newIndex).css( {left: 0} );
+//       $group.css( {left: 0} );
+//       currentIndex = newIndex;
+//     });
+//   } //end of move function
+//
+//
+//   function advance() {
+//     clearTimeout(timeout);
+//
+//     timeout = setTimeout(function() {
+//       if (currentIndex < ($slides.length - 1)) {
+//         move(currentIndex + 1);
+//       } else {
+//         move(0);
+//       }
+//     }, 4000);
+//   } // end of advance function
+//
+//   $.each($slides, function(index) {
+//     let $button = $('<button type="button" class="slide-btn">&bull;</button>');
+//     if (index === currentIndex) {
+//       $button.addClass('active');
+//     }
+//     $button.on('click', function() {
+//       move(index);
+//     }).appendTo($this.find('.slide-buttons'));
+//     buttonArray.push($button);
+//   });
+//
+//   advance();
+//
+// }); //end of slide function
 
 
 // Another option that works
