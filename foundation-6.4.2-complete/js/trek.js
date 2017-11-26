e1f1e2c050cda69612228e9eb8437c2e7a36b190
Tanisha <tanishahampden@gmail.com>
2017-11-26 15:55:00 -0800

    add styles

--- foundation-6.4.2-complete/js/trek.js
+++ foundation-6.4.2-complete/js/trek.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     $.get('https://trektravel.herokuapp.com/trips',
       response => {
         response.forEach(function(trip) {
-          let tripInfo = `<li><h3 data-id=${trip.id}>${trip.name}</h3><p>${trip.continent}</p></li>`
+          let tripInfo = `<li><p data-id=${trip.id}><strong>${trip.name}</strong> in ${trip.continent}</p></li>`
           $('ul').append(tripInfo);
         }); // end of forEach
 
@@ -21,13 +21,12 @@ $( document ).ready(function() {
     $.get(`https://trektravel.herokuapp.com/trips/${id}`,
       response => {
         let tripInfo = `
-        <h2> ${response.name} </h2>
-        <p> id: ${response.id} </p>
-        <p> continent: ${response.continent} </p>
-        <p> about: ${response.about} </p>
+        <h4>${response.name} in ${response.continent}</h4>
+        <p> This is a <strong>${response.weeks}</strong> week long trip</p>
         <p> category: ${response.category} </p>
-        <p> weeks: ${response.weeks} </p>
-        <p> cost: ${response.cost} </p>`;
+        <p> cost: ${response.cost} </p>
+        <p> description: ${response.about} </p>
+        <p> trip id : ${response.id} </p>`;
 
         let reserveButton = `<button type="button" name="button">Reserve Spot</button>`
 
@@ -69,7 +68,7 @@ $( document ).ready(function() {
 // });
 
   // EVENTS
-  $('ul').on('click', 'h3', function(){
+  $('ul').on('click', 'p', function(){
     let tripID = $(this).attr('data-id');
     loadTrip(tripID);
   });
