58e13811e89f267308d8a67c1deb5be936b1a794
Sarah Read-Brown <srbusiness2@gmail.com>
2017-11-25 21:22:31 -0800

    Started making the styles look like national geographic

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const successCallback = function(response){
   console.log(response);
   let name = $('#trip-form').data('name');
   // console.log(`Trip name: ${name}`);
-  $('#message').html(`You successfully reserved a spot on the following trip. \n ${name} `);
+  $('#message').html(`Congratulations! You successfully reserved a spot on ${name}!`);
 };
 
 $(document).ready(()=> {
   $('#reserve-form').hide();
+  $('#content').hide();
 
   let loadTrips = function loadTrips() {
     $.get(baseUrl, (response) => {
+      console.log(response);
       console.log('success!');
       response.forEach(function(trip) {
         let tripInfo = `<li><h2 data-id=${trip.id}>` + trip.name + '<h2></li>';
@@ -60,28 +62,29 @@ $(document).ready(()=> {
   $('#show-trips ul').on('click', 'h2', function(){
     let tripId = $(this).attr('data-id');
     loadTrip(tripId);
+
     $('html,body').animate({
       scrollTop: $("#anchor").offset().top},
       'slow');
   });
 
   $('#load').on('click', function(){
+    $('#content').show();
     loadTrips();
-    $('html,body').animate({ scrollTop: $("#show-trips").offset().top -30},'slow');
+    $('html,body').animate({ scrollTop: $("#show-trips").offset().top -20},'slow');
   });
 
   $('#trip-form').on('submit', function(event){
     event.preventDefault();
     let url = baseUrl + `/${$(this).data('id')}/reservations`;
     let formData = $('#trip-form').serialize();
-    let tripName = $(this).data('name')
-    console.log(`Trip Name: ${tripName}`);
-    console.log('formData');
+    let tripName = $(this).data('name');
     console.log(formData);
     $.post(url, formData,successCallback).fail((response) => {
       console.log("Didn't go so hot");
       $('#message').html(`There was an error in your reservation for  ${tripName}, please try again`);
     });
+    // When the user clicks the button, open the modal
     modal.style.display = "block";
   });
 
@@ -94,11 +97,6 @@ $(document).ready(()=> {
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
 
-  // When the user clicks the button, open the modal
-  // btn.onclick = function() {
-    // modal.style.display = "block";
-  // };
-
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
@@ -110,4 +108,17 @@ $(document).ready(()=> {
       modal.style.display = "none";
     }
   };
+
+  // $("#load").mouseover(function(){
+  //   $("#highlight").addClass("on").removeClass("off");
+  // });
+  // mouseover
+  $("#load").hover(function() {
+    $("#highlight").addClass("on").removeClass("off");
+  },
+
+  // mouse out
+  function() {
+    $('#highlight').addClass("off");
+  });
 });
