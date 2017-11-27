43fea8bd83c63f9255218dd1bd32971c341f8afa
murog <guillermina.muro@gmail.com>
2017-11-27 01:38:49 -0800

    added styling

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 $(document).ready(function() {
 $('#load-trips').click(function(event) {
   $.get('https://trektravel.herokuapp.com/trips',
@@ -6,39 +5,40 @@ $('#load-trips').click(function(event) {
     console.log('success!');
     console.log(response);
     for (let i = 0; i < response.length; i++) {
-      $('#trips').append(`<article id='trip-${response[i].id}'><h3>trip #${response[i].id}</h3></article>`);
+      $('#trips').append(`<article id='trip-${response[i].id}'><button class="dropdown button small-12">${response[i].name}</button></article>`);
 
-      $(`#trip-${response[i].id} > h3`).click(function(event) {
+      $(`#trip-${response[i].id} > button`).click(function(event) {
         console.log('you clicked');
         if (window.$(`#info-${response[i].id}`)[0])  {
-          console.log($(`#info-${response[i].id}`));
-          console.log('found context');
           $(`#info-${response[i].id}`).remove();
         } else {
         $.get(`https://trektravel.herokuapp.com/trips/${response[i].id}`,
         trip => {
-          console.log($(`#info-${trip.id}`));
-          $(`#trip-${trip.id}`).append(`<section id='info-${trip.id}'><button id='hide-${trip.id}'>hide me </button><table><tr><td>ID: ${trip.id}</td>
-            <td>NAME: ${trip.name}</td>
-            <td>CONTINENT:${trip.continent}</td>
-            <td>COST: ${trip.cost}</td>
-            <td>CATEGORY: ${trip.category}</td>
-            <td>WEEKS: ${trip.weeks}</td></tr></table>
-            <table>
-            <tr><td>ABOUT: ${trip.about}</td></tr></table>
+          $(`#trip-${trip.id}`).append(`<section class="row"><section id='info-${trip.id}' class='panel small-10 small-centered columns'><p>ID: ${trip.id}</p>
+            <p>NAME: ${trip.name}</p>
+            <p>CONTINENT:${trip.continent}</p>
+            <p>COST: ${trip.cost}</p>
+            <p>CATEGORY: ${trip.category}</p>
+            <p>WEEKS: ${trip.weeks}</p>
+            <p>ABOUT: ${trip.about}</p>
             <form action='https://trektravel.herokuapp.com/trips/${trip.id}/reservations' method='post' id='form-${trip.id}'>
             <section>
               <label>Name</label>
-              <input type="text" id="name" value="name"/>
+              <input type="text" id="name" placeholder="identification required"/>
             </section>
             <section>
               <label>Email</label>
-              <input type="text" id="email" value="email"/>
+              <input type="text" id="email" placeholder="electronic document transmission"/>
             </section>
             <section>
-              <button type="submit">Reserve a Spot for ${trip.name}</button>
+              <label>Age</label>
+              <input type="text" id="email" placeholder="age in relation to orbital period of Earth around solar body"/>
+            </section>
+            <section>
+              <button type="submit" class="button round">Reserve a Spot for ${trip.name}</button>
             </section>
             </form>
+            </section>
             </section>`);
             $('body').on('submit', 'form', function (e){
               e.preventDefault();
@@ -46,31 +46,23 @@ $('#load-trips').click(function(event) {
               const formData = $(this).serialize();
 
               $.post(url, formData, (response) => {
-                $('#message').html(`<p>Reserved your spot!</p>`);
+                $('#message').text('Reservation saved').show().fadeOut(9000);
               }).fail(() => {
                 $('#message').html('<p>reserving your spot failed aaarrroooo0o</p>');
               }).always(() => {
                 console.log('looks like you made a request to reserve a spot, how did that go?');
               });
             });
-            // $(`#hide-${trip.id}`).click(function(event) {
-            //   console.log('im hiding');
-            //   $(`#info-${trip.id}`).toggle();
-            // });
         });
 }
         })
-
       };
     });
   });
-
-  });
-    // .fail(function(){
-    //   console.log('failure');
-    //   $('#message').html('<p>Request failed no0o0</p>');
-    // })
-    // .always(function(){
-    //   console.log('always even if we have success or failure');
-    // }); // Note that this is where the semi-colon ends up
+  }).fail(function(){
+      console.log('failure');
+      $('#message').html('<p>Request failed no0o0</p>');
+    }).always(function(){
+      console.log('always even if we have success or failure');
+    }); // Note that this is where the semi-colon ends up
 // });
