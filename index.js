b70c6a8650b2bb77bcbe0a5b753931bd7e141e92
Sara C <srachandler@gmail.com>
2017-11-26 22:26:45 -0800

    styling finished, project working

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 $(document).ready(() => {
-
-  $('#load').click(function() {
+  $('#load').click(() => {
     $.get('https://trektravel.herokuapp.com/trips',
       (response) => {
         console.log(response);
-        response.forEach(function(trek) {
-          let trekInfo = `<li><h3 data-id=${trek.id}><a class="totrip"> ${trek.name} </a></h3><p> ${trek.continent}, ${trek.weeks}</li>`
+        response.forEach((trek) => {
+          const trekInfo = `<li><article class="columns float-center small-12 medium-4 large-3"><h3 data-id=${trek.id}><a class="totrip"> ${trek.name} </a></h3><p> Continent: ${trek.continent}, Number of weeks: ${trek.weeks}</article></li>`
 
           $('#trips ul').append(trekInfo);
+          $('#trips').show();
+          $('#trip').hide();
+          $('.message').hide();
         });
-
       })
-      .fail(function(response){
-          console.log(response);
-          $('#fail').html('<p>Request was unsuccessful</p>')
-        })
-        .always(function(){
-          console.log('always even if we have success or failure');
-        });
+      .fail(() => {
+        $('#fail').html('<p>Request was unsuccessful</p>')
+      })
   });
 
-  $('#trips ul').on('click', 'h3', function(){
+  $('#trips ul').on('click', 'h3', function () {
     let tripID = $(this).attr('data-id');
     $.get(`https://trektravel.herokuapp.com/trips/${tripID}`,
       (response) => {
@@ -32,6 +29,7 @@ $(document).ready(() => {
         <p> About: ${response.about} </p>
         <p> Weeks: ${response.weeks} </p>
         <p> Cost: ${response.cost} </p>
+        <p><h3> Reserve your spot now! </h3></p>
         <iframe name="hiddenFrame" class="hide"></iframe>
         <form action="https://trektravel.herokuapp.com/trips/${tripID}/reservations" method="post" target="hiddenFrame">
       <section>
@@ -53,16 +51,25 @@ $(document).ready(() => {
     </form>`;
 
         $('#trip').html(tripInfo);
-
+        $('#trips').hide();
+        $('#trip').show();
       })
-      .fail(function(response){
-          console.log(response);
-          $('#fail').html('<p>Request was unsuccessful</p>')
-        })
-        .always(function(){
-          console.log('always even if we have success or failure');
-        });
-
+      .fail((response) => {
+        console.log(response);
+        $('#fail').html('<p>Request was unsuccessful</p>');
+      });
   });
+  $('body').on('submit', 'form', (e) => {
+    e.preventDefault();
+    const url = $('form').attr('action');
+    const formData = $('form').serialize();
 
+    $.post(url, formData, () => {
+      $('.message').html('<h3>Trip successfully reserved!</h3>');
+      $('.message').show();
+    }).fail(() => {
+      $('.message').html('<h3>An Error Has Occured.</h3>');
+      $('.message').show();
+    });
+  });
 });
