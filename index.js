e4baacf4dad7b49f092dbf8df9685dbefd2f8099
murog <guillermina.muro@gmail.com>
2017-11-26 16:06:49 -0800

    form no longer redirects to api, flash message working, linked to foundation cdn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-
+$(document).ready(function() {
 $('#load-trips').click(function(event) {
   $.get('https://trektravel.herokuapp.com/trips',
   response => {
@@ -7,10 +7,17 @@ $('#load-trips').click(function(event) {
     console.log(response);
     for (let i = 0; i < response.length; i++) {
       $('#trips').append(`<article id='trip-${response[i].id}'><h3>trip #${response[i].id}</h3></article>`);
-      $(`#trip-${response[i].id}`).click(function(event) {
+
+      $(`#trip-${response[i].id} > h3`).click(function(event) {
+        if ($(`#info-${response[i].id}[0]`).length == 1) {
+          console.log($(`#info-${response[i].id}[0]`));
+          $(`#info-${response[i].id}`).toggle();
+        } else {
+
         $.get(`https://trektravel.herokuapp.com/trips/${response[i].id}`,
         trip => {
-          $(`#trip-${trip.id}`).append(`<table><tr><td>ID: ${trip.id}</td>
+          console.log($(`#info-${trip.id}`));
+          $(`#trip-${trip.id}`).append(`<section id='info-${trip.id}'><button id='hide-${trip.id}'>hide me </button><table><tr><td>ID: ${trip.id}</td>
             <td>NAME: ${trip.name}</td>
             <td>CONTINENT:${trip.continent}</td>
             <td>COST: ${trip.cost}</td>
@@ -18,16 +25,37 @@ $('#load-trips').click(function(event) {
             <td>WEEKS: ${trip.weeks}</td></tr></table>
             <table>
             <tr><td>ABOUT: ${trip.about}</td></tr></table>
-            <form action='https://trektravel.herokuapp.com/trips/${trip.id}/reservations' method='post'>
+            <form action='https://trektravel.herokuapp.com/trips/${trip.id}/reservations' method='post' id='form-${trip.id}'>
             <section>
             <button type="submit">Reserve a Spot for ${trip.name}</button>
             </section>
-            </form>`);
+            </form>
+            </section>`);
+            $('body').on('submit', 'form', function (e){
+              e.preventDefault();
+              const url = $(this).attr('action');
+              const formData = $(this).serialize();
+
+              $.post(url, formData, (response) => {
+                $('#message').html(`<p>Reserved your spot!</p>`);
+              }).fail(() => {
+                $('#message').html('<p>reserving your spot failed aaarrroooo0o</p>');
+              }).always(() => {
+                console.log('looks like you made a request to reserve a spot, how did that go?');
+              });
+            });
+            // $(`#hide-${trip.id}`).click(function(event) {
+            //   console.log('im hiding');
+            //   $(`#info-${trip.id}`).toggle();
+            // });
         });
+}
         })
 
       };
     });
+  });
+
   });
     // .fail(function(){
     //   console.log('failure');
