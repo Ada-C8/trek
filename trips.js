049ee0cd7bf83588ff25ac8b0a8d564d9fb74f06
Canaan West <canaan.west@gmail.com>
2017-11-26 18:28:38 -0800

    fixing index picture spacing

--- trips.js
+++ trips.js
@@ -3,6 +3,7 @@ $(document).ready(()=>{
   $('#bookingForm').hide();
   const url = 'https://trektravel.herokuapp.com/trips'
 
+
   $.get(url, response=> {
     let countries = []
     const allTrips = response;
@@ -27,7 +28,7 @@ $(document).ready(()=>{
       for (let i = 0; i < allTrips.length; i++){
         const trip = allTrips[i]
         if (trip.name !== "" && trip.name !== null){
-          $('#trips').append($(`<article><h3>${trip.name}</h3><p id="subDeets"> ${trip.continent} | ${trip.weeks} week(s)</p><img class="indexImg" src="http://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenery_01_hd_picture_166232.jpg" alt="generic issue"></article>`).attr('id', `${trip.id}`).addClass('tripListAll column small-12 large-6 float-right'));
+          $('#trips').append($(`<article><h3>${trip.name}</h3><p id="subDeets"> ${trip.continent} | ${trip.weeks} week(s)</p><img class="indexImg" src="http://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenery_01_hd_picture_166232.jpg" alt="generic issue"></article>`).attr('id', `${trip.id}`).addClass('tripListAll column small-12 large-6 float-left'));
         console.log(trip);}
       }
     })
@@ -96,7 +97,7 @@ $(document).ready(()=>{
         const trip = allTrips[i]
         if (trip.continent === this.id) {
           console.log(this.id)
-          $('#trips').append($(`<article><h3>${trip.name}</h3><p id="subDeets"> ${trip.continent} | ${trip.weeks} week(s)</p><img class="indexImg" src="http://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenery_01_hd_picture_166232.jpg" alt="generic issue"></article>`).attr('id', `${trip.id}`).addClass('tripListAll column small-12 large-6 float-right'));
+          $('#trips').append($(`<article><h3>${trip.name}</h3><p id="subDeets"> ${trip.continent} | ${trip.weeks} week(s)</p><img class="indexImg" src="http://images.all-free-download.com/images/graphiclarge/beautiful_natural_scenery_01_hd_picture_166232.jpg" alt="generic issue"></article>`).attr('id', `${trip.id}`).addClass('tripListAll column small-12 large-6 float-left'));
         }
       }
     })
