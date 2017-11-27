dca8d58141023e08d3767f638d6227b0c5255b83
Kayla Ecker <The Email Associated with Your Github Account>
2017-11-26 18:22:59 -0800

    changed var to let to limit scope of variables

--- trek.js
+++ trek.js
@@ -1,5 +1,5 @@
-var baseUrl = "https://trektravel.herokuapp.com/trips/";
-var clearPage = function () {
+let baseUrl = "https://trektravel.herokuapp.com/trips/";
+let clearPage = function () {
   $('main').empty();
   $('main').show();
 };
@@ -7,44 +7,44 @@ var clearPage = function () {
 $(document).ready(function () {
   $('main').hide();
 
-  var tripListTemplate = _.template($('#trip-list-template').html());
-  var showTripTemplate = _.template($('#show-trip-template').html());
+  let tripListTemplate = _.template($('#trip-list-template').html());
+  let showTripTemplate = _.template($('#show-trip-template').html());
 
-  var tripsCallback = function (response) {
+  let tripsCallback = function (response) {
     clearPage();
-    var generatedHtml = tripListTemplate({ trips: response });
+    let generatedHtml = tripListTemplate({ trips: response });
     $('main').append(generatedHtml);
   };
 
-  var tripCallback = function (response) {
+  let tripCallback = function (response) {
     clearPage();
-    var reservationUrl = baseUrl + response.id + "/reserve";
-    var generatedHtml = showTripTemplate({
+    let reservationUrl = baseUrl + response.id + "/reserve";
+    let generatedHtml = showTripTemplate({
       trip: response,
       url: reservationUrl
     });
     $('main').append(generatedHtml);
   };
 
-  var failCallback = function (response) {
+  let failCallback = function (response) {
     $('main').prepend("<p id='callout alert'>AJAX Request Failed!</p>");
   };
 
   $('#get-trips').click(function() {
-    var url = baseUrl;
+    let url = baseUrl;
     $.get(url, tripsCallback).fail(failCallback);
   });
 
   $('main').on('click', '#trip-list a', function () {
-    var url = baseUrl + $(this).attr('id');
+    let url = baseUrl + $(this).attr('id');
     $.get(url, tripCallback).fail(failCallback);
   });
 
   $('form').submit(function (event) {
     event.preventDefault();
 
-    var url = $('form').attr('action');
-    var formData = $('form').serialize();
+    let url = $('form').attr('action');
+    let formData = $('form').serialize();
 
     $.post(url, formData, function (response) {
       $("<p class='callout success'>Reserved Successfully!</p>").insertAfter('h3');
